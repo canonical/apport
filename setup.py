813dca21d94b47b06a75526ba15e2a088cd78386
Martin Pitt <martin@piware.de>
2006-07-20 14:37:33 +0200

    add setup.py for the library

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='crash-reporter',
+      version='0.1',
+      author='Martin Pitt',
+      author_email='martin.pitt@ubuntu.com',
+      maintainer='Martin Pitt',
+      maintainer_email='martin.pitt@ubuntu.com',
+      url='http://www.ubuntu.com',
+      license='gpl',
+      description='read, write, and modify problem reports',
+      py_modules=['problem_report'],
+      )
