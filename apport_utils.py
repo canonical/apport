d5a82f0a1d9e8331cc0010ceb1eac44eb79deaf3
Martin Pitt <martin@piware.de>
2006-08-08 20:07:49 +0200

    apport_utils.py: add proper implementation for mark_report_seen()

--- apport_utils.py
+++ apport_utils.py
@@ -45,7 +45,23 @@ def seen_report(report):
 def mark_report_seen(report):
     '''Mark given report file as seen.'''
 
-    pass # nothing to be done with current stat atime implementation
+    # change the file's access time until it stat's different than the mtime.
+    # This might take a while if we only have 1-second resolution. Time out
+    # after 1.2 seconds.
+
+    timeout = 12
+    while timeout > 0:
+	f = open(report)
+	f.read(1)
+	f.close()
+	st = os.stat(report)
+	if st.st_atime > st.st_mtime:
+	    break
+	time.sleep(0.1)
+	timeout -= 1
+
+    if timeout == 0:
+	raise OSError, 'could not modify atime of report file ' + report
 
 def get_all_reports():
     '''Return a list with all report files which are accessible to the calling
