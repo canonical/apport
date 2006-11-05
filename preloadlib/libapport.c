8394d524cb85000115043f76e3a8e432e1bb322f
Martin Pitt <martin@piware.de>
2006-11-05 07:10:15 -0800

    * preloadlib: Support -DPIPE_CORE mode which emulates the   pipe-in-core_pattern mode of kernel 2.6.19.

--- preloadlib/libapport.c
+++ preloadlib/libapport.c
@@ -4,6 +4,10 @@
  * calls 'AGENTPATH <signal> <pid> <core dump>' upon them.
  * This library can either be linked to a program or used with
  * LD_PRELOAD=libapport.so.
+ * 
+ * If PIPE_CORE is defined, the core dump is piped to the agent's STDIN
+ * (similar to the pipe-in-core_pattern feature of Linux 2.6.19), otherwise the
+ * path is passed to the agent and the REMOVE_CORE environment variable is set.
  *
  * Copyright (c) 2006 Canonical Ltd.
  * Author: Martin Pitt <martin.pitt@ubuntu.com>
@@ -18,6 +22,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -37,11 +42,22 @@ void sighandler( int signum )
     char ssig[20], spid[20];
     sprintf( ssig, "%i", signum );
     sprintf( spid, "%i", getpid() );
+    char core_arg[PATH_MAX];
     char corepath[PATH_MAX];
     char *core = NULL;
     int status;
 
-    snprintf( corepath, sizeof(corepath), "%s.%s", "core", spid );
+#ifdef PIPE_CORE
+    strcpy( corepath, "/tmp/core.XXXXXX" );
+    status = mkstemp( corepath );
+    if( status < 0 ) {
+	perror( "mkstemp" );
+	goto out;
+    }
+    close( status );
+#else
+    snprintf( corepath, sizeof(corepath), "core.%s", spid );
+#endif
 
     // generate core file
     pid_t pid = fork();
@@ -54,7 +70,8 @@ void sighandler( int signum )
 	if( devnull > 0 )
 	    dup2(devnull, 1);
 	    dup2(devnull, 2);
-	execl( "/usr/bin/gcore", "/usr/bin/gcore", "-o", "core", spid, NULL );
+	snprintf( core_arg, sizeof( core_arg ), "generate-core-file %s", corepath );
+	execl( "/usr/bin/gdb", "/usr/bin/gdb", "--batch", "--ex", core_arg, "--pid", spid, NULL );
 	perror( "Error: could not execute gcore" );
 	goto out;
     }
@@ -67,12 +84,25 @@ void sighandler( int signum )
     /* only pass the core file if gcore succeeded */
     if( WIFEXITED( status ) && WEXITSTATUS( status ) == 0 ) {
 	core = corepath;
+#ifndef PIPE_CORE
 	setenv("REMOVE_CORE", "1", 1);
+#endif
     }
 
     pid = fork();
     if( pid == 0 ) {
 	int devnull = open( "/dev/null", O_WRONLY );
+#ifdef PIPE_CORE
+	int corepipe;
+	if( core ) {
+	    corepipe = open( corepath, O_RDONLY );
+	    unlink( corepath );
+	    if( corepipe > 0 ) {
+		dup2( corepipe, 0 );
+		core = "-";
+	    }
+	}
+#endif
 	if( devnull > 0 )
 	    dup2(devnull, 2);
 	if( execl( AGENTPATH, AGENTPATH, spid, ssig, core, NULL ) == -1 )
