570661b0c27b9d9d1b14972f623dae24d5026db7
Martin Pitt <martin.pitt@ubuntu.com>
2007-06-08 13:26:49 +0200

    * apport/crashdb.py: Add method interfaces for crash duplicate detection.

--- apport/crashdb.py
+++ apport/crashdb.py
@@ -26,6 +26,7 @@ class CrashDatabase:
         self.auth_file = auth_file
         self.options = options
         self.bugpattern_baseurl = bugpattern_baseurl
+        self.duplicate_db = None
 
     def get_bugpattern_baseurl(self):
         '''Return the base URL for bug patterns.
@@ -34,6 +35,74 @@ class CrashDatabase:
         function returns None, bug patterns are disabled.'''
 
         return self.bugpattern_baseurl
+
+    #
+    # API for duplicate detection
+    #
+
+    def init_duplicate_db(self, path):
+        '''Initialize duplicate database.
+
+        path specifies an SQLite database. It will be created if it does not
+        exist yet.'''
+
+        from pysqlite2 import dbapi2
+
+        assert dbapi2.paramstyle == 'qmark', \
+            'this module assumes qmark dbapi parameter style'
+
+        init = not os.path.exists(path)
+        self.duplicate_db = dbapi2.connect(path)
+
+        if init:
+            cur = db.cursor()
+            cur.execute('''CREATE TABLE crashes (
+                signature VARCHAR(255) NOT NULL,
+                crash_id INTEGER NOT NULL,
+                fixed_version VARCHAR(50))''')
+            self.duplicate_db.commit()
+
+    def check_duplicate(self, report):
+        '''Check whether the crash is already known.
+
+        If the crash is new, it will be added to the duplicate database and the
+        function returns None. If the crash is already known, the function
+        returns a pair (crash_id, fixed_version), where fixed_version might be
+        None if the crash is not fixed in the latest version yet.'''
+
+        assert self.duplicate_db, 'init_duplicate_db() needs to be called before'
+
+        raise Exception, 'not yet implemented'
+
+    def duplicate_db_fixed(self, id, version):
+        '''Mark given crash ID as fixed in the duplicate database.
+        
+        version specifies the package version the crash was fixed in.'''
+
+        assert self.duplicate_db, 'init_duplicate_db() needs to be called before'
+
+        raise Exception, 'not yet implemented'
+
+    def duplicate_db_remove(self, id):
+        '''Remove crash from the duplicate database (because it got rejected or
+        manually duplicated).'''
+
+        assert self.duplicate_db, 'init_duplicate_db() needs to be called before'
+
+        raise Exception, 'not yet implemented'
+
+    def duplicate_db_consolidate(self):
+        '''Update the duplicate database status to the reality of the crash
+        database.
+
+        This uses get_status_list() to get the status of all crashes. IDs which
+        do not occur there any more are removed from the duplicate db, and
+        crashes which got fixed since the last run are marked as such in the
+        database.'''
+
+        assert self.duplicate_db, 'init_duplicate_db() needs to be called before'
+
+        raise Exception, 'not yet implemented'
 
     #
     # Abstract functions that need to be implemented by subclasses
@@ -73,6 +142,22 @@ class CrashDatabase:
         it.'''
 
         raise Exception, 'this method must be implemented by a concrete subclass'
+
+    def get_status_list(self):
+        '''Return a mapping 'id -> fixed_version' of all currently tracked crashes.
+
+        The keys are integers (crash IDs), the values are 'None' for unfixed
+        crashes or the package version the crash was fixed in for resolved
+        crashes. The list must not contain bugs which were rejected or manually
+        marked as duplicate.
+        
+        This is a very expensive operation and should not be used too often.
+        
+        This function should make sure that the returned map is consistent. If
+        there are any errors with connecting to the crash database, it should
+        raise an exception (preferably IOError).'''
+
+        raise NotImplementedError, 'this method must be implemented by a concrete subclass'
 
 #
 # factory 
