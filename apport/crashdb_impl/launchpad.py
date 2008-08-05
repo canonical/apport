0e4b7dea6aff39833a8d7bbe0f3f667dd9b711bd
Martin Pitt <martin.pitt@canonical.com>
2008-08-05 15:50:12 +0200

    apport/crashdb_impl/launchpad.py, download(): Support reading bugs with the "--- " separator instead of "ProblemType: ". Launchpad doesn't create bugs that way ATM, but at least we have the reading part implemented now.

--- apport/crashdb_impl/launchpad.py
+++ apport/crashdb_impl/launchpad.py
@@ -121,12 +121,26 @@ class CrashDatabase(apport.crashdb.CrashDatabase):
 
             # parse out fields from summary
             m = re.search(r"(ProblemType:.*)$", b.description_raw, re.S)
+            if not m:
+                m = re.search(r"^--- \r?$[\r\n]*(.*)", b.description_raw, re.M | re.S)
             assert m, 'bug description must contain standard apport format data'
+
             description = m.group(1).replace("\xc2\xa0", " ")
             
             report.load(StringIO(description))
 
             report['Date'] = b.date.ctime()
+            if 'ProblemType' not in report:
+                if 'apport-bug' in b.tags:
+                    report['ProblemType'] = 'Bug'
+                elif 'apport-crash' in b.tags:
+                    report['ProblemType'] = 'Crash'
+                elif 'apport-kernel' in b.tags:
+                    report['ProblemType'] = 'Kernel'
+                elif 'apport-package' in b.tags:
+                    report['ProblemType'] = 'Package'
+                else:
+                    raise ValueError, 'cannot determine ProblemType from tags: ' + str(b.tags)
 
             for att in b.attachments.filter(lambda a: re.match(
                     "Dependencies.txt|CoreDump.gz|ProcMaps.txt|Traceback.txt",
