8768009eaebb1dfdf46ded4de181da1881fb7cf6
Matt Zimmerman <mdz@canonical.com>
2008-09-18 23:24:34 +0100

    Rename kernel_hook to kernel_crashdump (there are other kernel hooks)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(name='apport',
                   ('/etc/apport/', ['crashdb.conf'])]+mo_files,
       scripts=['bin/apport', 'bin/apport-checkreports', 'bin/apport-retrace',
           'bin/apport-unpack', 'bin/apport-chroot', 'bin/package_hook',
-          'bin/kernel_hook', 'bin/gcc_ice_hook', 'gtk/apport-gtk',
+          'bin/kernel_crashdump', 'bin/gcc_ice_hook', 'gtk/apport-gtk',
           'qt4/apport-qt', 'cli/apport-cli', 'bin/dupdb-admin'],
       packages=['apport', 'apport.crashdb_impl']
       )
