08dcb1b6a17f5f87bee2201c97957a7a850c49a2
Martin Pitt <martin.pitt@canonical.com>
2009-02-23 13:00:24 +0100

    package-hooks/source_apport.py: Robustify by using hookutils, and avoid stat errors if /var/crash/* does not exist.

--- package-hooks/source_apport.py
+++ package-hooks/source_apport.py
@@ -8,10 +8,12 @@ Author: Martin Pitt <martin.pitt@ubuntu.com>'''
 APPORT_LOG = '/var/log/apport.log'
 
 import os.path, subprocess
+from glob import glob
+import apport.hookutils
 
 def add_info(report):
-    if os.path.exists(APPORT_LOG):
-        report['ApportLog'] = open(APPORT_LOG).read()
-    stat = subprocess.Popen('stat -c %a:%u:%g:%s:%y:%x:%n /var/crash/*',
-        shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    report['CrashReports'] = stat.communicate()[0]
+    apport.hookutils.attach_file_if_exists(report, APPORT_LOG, 'ApportLog')
+    reports = glob('/var/crash/*')
+    if reports:
+        report['CrashReports'] = apport.hookutils.command_output(
+            ['stat', '-c', '%a:%u:%g:%s:%y:%x:%n'] + reports)
