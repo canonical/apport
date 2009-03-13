88758ce73a40343cb267bcb45c26cb39ec90bf32
Martin Pitt <martin.pitt@canonical.com>
2009-03-13 01:21:08 +0100

    integrate storeblob

--- apport/crashdb_impl/launchpad.py
+++ apport/crashdb_impl/launchpad.py
@@ -12,7 +12,6 @@ the full text of the license.
 '''
 TODO:
     * missing API:
-        - storeblob LP: #315358
         - changing target of task LP #309182
         
     * related bugs
@@ -25,8 +24,6 @@ from cStringIO import StringIO
 from launchpadlib.errors import HTTPError
 from launchpadlib.launchpad import Launchpad, STAGING_SERVICE_ROOT, EDGE_SERVICE_ROOT
 from launchpadlib.credentials import Credentials
-
-import launchpadbugs.storeblob
 
 import apport.crashdb
 import apport
@@ -152,7 +149,7 @@ class CrashDatabase(apport.crashdb.CrashDatabase):
         mime.flush()
         mime.seek(0)
 
-        ticket = launchpadbugs.storeblob.upload(mime, progress_callback, 
+        ticket = upload_blob(mime, progress_callback,
                 staging=self.options.get('staging', False))
         assert ticket
         return ticket
@@ -522,6 +519,89 @@ in a dependent package.' % master,
         #use a url hack here, it is faster
         person = '%s~ubuntu-crashes-universe' %self.launchpad._root_uri
         bug.subscribe(person=person)
+
+#
+# Launchpad storeblob API (should go into launchpadlib, see LP #315358)
+#
+
+import multipartpost_handler, urllib2, time, httplib
+import socket
+
+_https_upload_callback = None
+
+#
+# This progress code is based on KodakLoader by Jason Hildebrand
+# <jason@opensky.ca>. See http://www.opensky.ca/~jdhildeb/software/kodakloader/
+# for details.
+class HTTPSProgressConnection(httplib.HTTPSConnection):
+    '''Implement a HTTPSConnection with an optional callback function for
+    upload progress.'''
+
+    def send(self, data):
+        global _https_upload_callback
+
+        # if callback has not been set, call the old method
+        if not _https_upload_callback:
+            httplib.HTTPSConnection.send(self, data)
+            return
+
+        sent = 0
+        total = len(data)
+        chunksize = 1024
+        while sent < total:
+            _https_upload_callback(sent, total)
+            t1 = time.time()
+            httplib.HTTPSConnection.send(self, data[sent:sent+chunksize])
+            sent += chunksize
+            t2 = time.time()
+
+            # adjust chunksize so that it takes between .5 and 2 
+            # seconds to send a chunk
+            if t2 - t1 < .5:
+                chunksize *= 2
+            elif t2 - t1 > 2:
+                chunksize /= 2
+
+class HTTPSProgressHandler(urllib2.HTTPSHandler):
+
+    def https_open(self, req):
+        return self.do_open(HTTPSProgressConnection, req)
+
+def upload_blob(blob, progress_callback = None, staging=False):
+    '''Upload blob (file-like object) to Launchpad.
+
+    progress_callback can be set to a function(sent, total) which is regularly
+    called with the number of bytes already sent and total number of bytes to
+    send. It is called every 0.5 to 2 seconds (dynamically adapted to upload
+    bandwidth).
+
+    Return None on error, or the ticket number on success.
+
+    By default this uses the production Launchpad instance. Set staging=True to
+    use staging.launchpad.net (for testing).
+    '''
+    # working around LP: #314212
+    # python-apt sets a 2 seconds timeout in jaunty which results in
+    # failing uploads. To workaround this temporary set timeout to None
+    # (no timeout)
+    old_timeout = socket.getdefaulttimeout()
+    socket.setdefaulttimeout(30)
+    ticket = None
+
+    global _https_upload_callback
+    _https_upload_callback = progress_callback
+
+    opener = urllib2.build_opener(HTTPSProgressHandler, multipartpost_handler.MultipartPostHandler)
+    if staging:
+        url = 'https://staging.launchpad.net/+storeblob'
+    else:
+        url = 'https://launchpad.net/+storeblob'
+    result = opener.open(url,
+        { 'FORM_SUBMIT': '1', 'field.blob': blob })
+    ticket = result.info().get('X-Launchpad-Blob-Token')
+    socket.setdefaulttimeout(old_timeout)
+
+    return ticket
 
 #
 # Unit tests
