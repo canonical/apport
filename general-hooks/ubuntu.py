8beed73b7813795692cadaa5b316231604543619
Matt Zimmerman <mdz@canonical.com>
2009-03-30 20:31:49 +0100

    * apport/hookutils.py: add attach_media_build to include information about   the build of installation media in use (i.e. in a casper live CD   environment) * general-hooks/ubuntu.py: use attach_media_build (LP: #351781)

---
+++ general-hooks/ubuntu.py
@@ -0,0 +1,17 @@
+'''Attach generally useful information, not specific to any package.
+
+Copyright (C) 2009 Canonical Ltd.
+Author: Matt Zimmerman <mdz@canonical.com>
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+the full text of the license.
+'''
+
+import apport.packaging
+from apport.hookutils import *
+
+def add_info(report):
+    attach_media_build(report)
