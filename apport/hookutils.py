3afe0a781c8263fed458c6ce74413c76ea250e13
Marco Rodrigues <gothicx@sapo.pt>
2009-10-25 20:19:28 +0000

    Hide the wifi Encryption key

--- apport/hookutils.py
+++ apport/hookutils.py
@@ -378,7 +378,7 @@ def attach_wifi(report):
     '''Attach wireless (WiFi) network information to report.'''
 
     report['WifiSyslog'] = recent_syslog(re.compile(r'(NetworkManager|modem-manager|dhclient|kernel):'))
-    report['IwConfig'] = command_output(['iwconfig'])
+    report['IwConfig'] = re.sub('Encryption key:(.*)', 'Encryption key: <hidden>', command_output(['iwconfig']))
     report['RfKill'] = command_output(['rfkill', 'list'])
     report['CRDA'] = command_output(['iw', 'reg', 'get'])
 
