d3b2d7cb5713457ef69e40853e11b7887c0db9ce
Matt Zimmerman <mdz@ubuntu.com>
2010-03-30 13:41:52 +0100

    whitespace

--- java/crash.java
+++ java/crash.java
@@ -2,7 +2,7 @@ import com.ubuntu.apport.*;
 
 class crash {
     public static void main(String[] args) {
-		com.ubuntu.apport.ApportUncaughtExceptionHandler.install();
-		throw new RuntimeException("Can't catch this");
+        com.ubuntu.apport.ApportUncaughtExceptionHandler.install();
+        throw new RuntimeException("Can't catch this");
     }
 }
