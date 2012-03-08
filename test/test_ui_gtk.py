da04da6d44c7bc33e367adf3e013079980dd5bd9
Evan Dandrea <evan.dandrea@canonical.com>
2012-03-08 16:43:32 +0000

    Fallback if we don't know the distribution release, and account for that in the tests.

--- test/test_ui_gtk.py
+++ test/test_ui_gtk.py
@@ -57,6 +57,7 @@ class T(unittest.TestCase):
         self.app.report['ExecutablePath'] = '/bin/bash'
         self.app.report['Signal'] = '11'
         self.app.report['CoreDump'] = ''
+        self.app.report['DistroRelease'] = self.distro
         with open(self.app.report_file, 'w') as f:
             self.app.report.write(f)
 
