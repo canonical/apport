92fb0264ffc01a65e0ccf23f50d7c32842d8df3b
Evan Dandrea <evan.dandrea@canonical.com>
2012-07-12 15:11:46 +0100

    Refactor test for recoverable problems. Test for incomplete data.

--- test/test_recoverable_problem.py
+++ test/test_recoverable_problem.py
@@ -25,14 +25,7 @@ class T(unittest.TestCase):
         self.addCleanup(shutil.rmtree, self.report_dir)
         os.environ['APPORT_REPORT_DIR'] = self.report_dir
 
-    def test_foo(self):
-        (r, w) = os.pipe()
-        with os.fdopen(r, 'r') as r:
-            with os.fdopen(w, 'w') as w:
-                proc = subprocess.Popen(['apport-recoverable-problem'], stdin=r, close_fds=True)
-                w.write('hello\0there')
-                w.flush()
-        proc.communicate()
+    def wait_for_report(self):
         cwd = os.getcwd().replace('/', '_')
         base = sys.argv[0].replace('/', '_')
         path = '%s_%s.%d.crash' % (cwd, base, os.getuid())
@@ -42,11 +35,36 @@ class T(unittest.TestCase):
             time.sleep(1)
             seconds += 1
             self.assertTrue(seconds < 10, 'timeout while waiting for %s to be created.' % path)
+        return path
+
+    def call_recoverable_problem(self, data):
+        (r, w) = os.pipe()
+        cmd = ['apport-recoverable-problem']
+        with os.fdopen(r, 'r') as r:
+            with os.fdopen(w, 'w') as w:
+                proc = subprocess.Popen(cmd, stdin=r, close_fds=True)
+                w.write(data)
+                w.flush()
+        proc.communicate()
+        if proc.returncode != 0:
+            raise subprocess.CalledProcessError(proc.returncode, cmd[0])
+
+    def test_recoverable_problem(self):
+        self.call_recoverable_problem('hello\0there')
+        path = self.wait_for_report()
         with open(path, 'rb') as report_path:
             report = apport.report.Report()
             report.load(report_path)
             self.assertEqual(report['hello'], 'there')
             self.assertTrue('Pid:\t%d' % os.getpid() in report['ProcStatus'])
+
+    def test_incomplete_data(self):
+        self.assertRaises(subprocess.CalledProcessError,
+                          self.call_recoverable_problem, 'hello')
+
+        self.assertRaises(subprocess.CalledProcessError,
+                          self.call_recoverable_problem,
+                          'hello\0there\0extraneous')
 
 
 with open('/proc/sys/kernel/core_pattern') as f:
