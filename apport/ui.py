4ee9c3e3c841a6baefbf497fa4238d7c3696100f
Martin Pitt <martin.pitt@canonical.com>
2013-09-27 05:54:01 +0200

    release 2.12.5

--- apport/ui.py
+++ apport/ui.py
@@ -13,7 +13,7 @@ implementation (like GTK, Qt, or CLI).
 # option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
 # the full text of the license.
 
-__version__ = '2.12.4'
+__version__ = '2.12.5'
 
 import glob, sys, os.path, optparse, traceback, locale, gettext, re
 import errno, zlib
