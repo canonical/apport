559f74b7b85f607b950fdaffa29b3f86d5720677
Martin Pitt <martin.pitt@canonical.com>
2016-12-10 12:41:30 +0100

    test_problem_report.py: Fix exception handling test, previously used duplicate keys

--- test/test_problem_report.py
+++ test/test_problem_report.py
@@ -288,9 +288,10 @@ Last: foo
         report.seek(0)
 
         self.assertRaises(IOError, pr.extract_keys, report, 'Bin', os.path.join(self.workdir, 'nonexistant'))
-        # Test exception handling : Non-binary and inexistant key
-        tests = {ValueError: 'Txt', ValueError: ['Foo', 'Txt'], KeyError: 'Bar', KeyError: ['Foo', 'Bar']}
-        for exc, keys_arg in tests.items():
+        # Test exception handling: Non-binary and nonexistent key
+        tests = [(ValueError, 'Txt'), (ValueError, ['Foo', 'Txt']),
+                 (KeyError, 'Bar'), (KeyError, ['Foo', 'Bar'])]
+        for exc, keys_arg in tests:
             report.seek(0)
             self.assertRaises(exc, pr.extract_keys, report, keys_arg, self.workdir)
 
