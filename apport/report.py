dd7adeba51ca052e89cb0806f0173829b229e617
Steve Langasek <steve.langasek@canonical.com>
2019-02-14 21:35:50 -0800

    Fix to make value_keys the same length as the gdb output

--- apport/report.py
+++ apport/report.py
@@ -728,6 +728,7 @@ class Report(problem_report.ProblemReport):
             value_keys.append(name)
             gdb_cmd += ['--ex', 'p -99', '--ex', cmd]
         # End with our separator, ensures gdb's return code is as expected
+        value_keys.append('separator')
         gdb_cmd += ['--ex', 'p -99']
 
         out = _command_output(gdb_cmd, env=environ).decode('UTF-8', errors='replace')
