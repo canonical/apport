canonical/apport

commit a32fb956c1454a99530764c0edd79e3724f52002

Grab a slice of JournalErrors around the crash time
For gnome-shell the desktop team wants the journal errors in the seconds
after the crash to see the Javascript stack dump (if any). But for some
other crashes they need to also see what happened just before the crash.

The crash data collection can happen after the crash, not immediately at
the time of the crash. This will lead to the `JournalErrors` entries to
show the logs seconds after the crash.

Grab a slice of JournalErrors around the crash time (if available)
instead of 1000 lines.

Bug: https://launchpad.net/bugs/1962454
Restructed-by: Benjamin Drung <bdrung@ubuntu.com>

apport/hookutils.py
@@ -725,6 +725,29 @@ def attach_gsettings_package(report, package):
         attach_gsettings_schema(report, schema)
 
 
+def attach_journal_errors(report, time_window=10) -> None:
+    '''Attach journal warnings and errors.
+
+    If the report contains a date, get the journal logs around that
+    date (plus/minus the time_window in seconds). Otherwise attach the
+    latest 1000 journal logs since the last boot.
+    '''
+
+    if not os.path.exists('/run/systemd/system'):
+        return
+
+    crash_time = report.get_date()
+    if crash_time:
+        before_crash = crash_time - datetime.timedelta(seconds=time_window)
+        after_crash = crash_time + datetime.timedelta(seconds=time_window)
+        args = [f'--since={before_crash}', f'--until={after_crash}']
+    else:
+        args = ['-b', '--lines=1000']
+    report['JournalErrors'] = command_output(
+        ['journalctl', '--priority=warning'] + args
+    )
+
+
 def attach_network(report):
     '''Attach generic network-related information to report.'''
 

tests/test_hookutils.py
@@ -191,6 +191,54 @@ class T(unittest.TestCase):
         apport.hookutils.attach_file_if_exists(report, '/etc/../etc/passwd')
         self.assertEqual(list(report), [])
 
+    @unittest.mock.patch("subprocess.Popen")
+    @unittest.mock.patch(
+        "os.path.exists", unittest.mock.MagicMock(return_value=True)
+    )
+    def test_attach_journal_errors_with_date(self, popen_mock):
+        popen_mock.return_value.returncode = 0
+        popen_mock.return_value.communicate.return_value = (
+            b"journalctl output",
+            b"",
+        )
+
+        report = apport.Report(date="Wed May 18 18:31:08 2022")
+        apport.hookutils.attach_journal_errors(report)
+
+        self.assertEqual(popen_mock.call_count, 1)
+        self.assertEqual(report.get("JournalErrors"), "journalctl output")
+        self.assertEqual(
+            popen_mock.call_args[0][0],
+            [
+                "journalctl",
+                "--priority=warning",
+                "--since=2022-05-18 18:30:58",
+                "--until=2022-05-18 18:31:18",
+            ],
+        )
+
+    @unittest.mock.patch("subprocess.Popen")
+    @unittest.mock.patch(
+        "os.path.exists", unittest.mock.MagicMock(return_value=True)
+    )
+    def test_attach_journal_errors_without_date(self, popen_mock):
+        popen_mock.return_value.returncode = 0
+        popen_mock.return_value.communicate.return_value = (
+            b"journalctl output",
+            b"",
+        )
+
+        report = apport.Report()
+        del report["Date"]
+        apport.hookutils.attach_journal_errors(report)
+
+        self.assertEqual(popen_mock.call_count, 1)
+        self.assertEqual(report.get("JournalErrors"), "journalctl output")
+        self.assertEqual(
+            popen_mock.call_args[0][0],
+            ["journalctl", "--priority=warning", "-b", "--lines=1000"],
+        )
+
     def test_path_to_key(self):
         '''transforming a file path to a valid report key'''
 

data/general-hooks/generic.py
@@ -88,9 +88,7 @@ Do you want to continue the report process anyway?
 
     # log errors
     if report['ProblemType'] == 'Crash':
-        if os.path.exists('/run/systemd/system'):
-            report['JournalErrors'] = apport.hookutils.command_output(
-                ['journalctl', '-b', '--priority=warning', '--lines=1000'])
+        apport.hookutils.attach_journal_errors(report)
 
 
 if __name__ == '__main__':

commit 9959a0addf7696826ee08b5851ac4b9a87d11da0

Replace ProblemReport.get_date by .get_timestamp
Apport 2.21.0-0ubuntu2 fails:

```
Traceback (most recent call last):
  File "/usr/lib/python3/dist-packages/apport/report.py", line 205, in _run_hook
    symb['add_info'](report, ui)
  File "/usr/share/apport/general-hooks/generic.py", line 94, in add_info
    apport.hookutils.attach_journal_errors(report)
  File "/usr/lib/python3/dist-packages/apport/hookutils.py", line 740, in attach_journal_errors
    crash_time = report.get_date()
  File "/usr/lib/python3/dist-packages/problem_report.py", line 268, in get_date
    return datetime.datetime.strptime(self['Date'], '%a %b %d %H:%M:%S %Y')
  File "/usr/lib/python3.10/_strptime.py", line 568, in _strptime_datetime
    tt, fraction, gmtoff_fraction = _strptime(data_string, format)
  File "/usr/lib/python3.10/_strptime.py", line 349, in _strptime
    raise ValueError("time data %r does not match format %r" %
ValueError: time data 'Sun Jun 12 19:52:55 2022' does not match format '%a %b %d %H:%M:%S %Y'
```

`Report.get_timestamp` does the same than `ProblemReport.get_date`
except it handles the locale correctly and returns a timestamp instead
of a datetime object. So move `get_timestamp` from `Report` to the
parent class `ProblemReport` and replace the single usage of
`ProblemReport.get_date` by `ProblemReport.get_timestamp`. Let
`ProblemReport.get_timestamp` return an `int` instead of `float` since
the report date granularity is seconds. Also ensure that
`ProblemReport.get_timestamp` is used in all possible cases.

Bug: https://launchpad.net/bugs/1978487
Fixes: a32fb956c145 ("Grab a slice of JournalErrors around the crash time")
Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

.github/workflows/ci.yaml
@@ -49,8 +49,10 @@ jobs:
         run: >
           apt-get update
           && apt-get install --no-install-recommends --yes
-          pkg-config python3 python3-apt python3-distutils-extra python3-pytest
-          python3-pytest-cov
+          locales pkg-config python3 python3-apt python3-distutils-extra
+          python3-pytest python3-pytest-cov
+      - name: Enable German locale
+        run: sed -i 's/^# de_DE/de_DE/g' /etc/locale.gen && locale-gen
       - name: Build
         run: ./setup.py build
       - name: Run unit tests

apport/hookutils.py
@@ -15,7 +15,6 @@
 import base64
 import datetime
 import glob
-import locale
 import os
 import re
 import select
@@ -24,7 +23,6 @@ import stat
 import subprocess
 import sys
 import tempfile
-import time
 
 import apport
 import apport.fileutils
@@ -735,11 +733,11 @@ def attach_journal_errors(report, time_window=10) -> None:
     if not os.path.exists('/run/systemd/system'):
         return
 
-    crash_time = report.get_date()
-    if crash_time:
-        before_crash = crash_time - datetime.timedelta(seconds=time_window)
-        after_crash = crash_time + datetime.timedelta(seconds=time_window)
-        args = [f'--since={before_crash}', f'--until={after_crash}']
+    crash_timestamp = report.get_timestamp()
+    if crash_timestamp:
+        before_crash = crash_timestamp - time_window
+        after_crash = crash_timestamp + time_window
+        args = [f'--since=@{before_crash}', f'--until=@{after_crash}']
     else:
         args = ['-b', '--lines=1000']
     report['JournalErrors'] = command_output(
@@ -987,17 +985,8 @@ def in_session_of_problem(report):
             else:
                 return None
 
-    # report time is in local TZ
-    orig_ctime = locale.getlocale(locale.LC_TIME)
-    try:
-        try:
-            locale.setlocale(locale.LC_TIME, 'C')
-            report_time = time.mktime(time.strptime(report['Date']))
-        except KeyError:
-            return None
-        finally:
-            locale.setlocale(locale.LC_TIME, orig_ctime)
-    except locale.Error:
+    report_time = report.get_timestamp()
+    if report_time is None:
         return None
 
     # determine session creation time

apport/report.py
@@ -16,7 +16,6 @@ import glob
 import grp
 import imp
 import io
-import locale
 import os
 import pwd
 import re
@@ -25,7 +24,6 @@ import stat
 import subprocess
 import sys
 import tempfile
-import time
 import traceback
 import xml.dom
 import xml.dom.minidom
@@ -1805,21 +1803,3 @@ class Report(problem_report.ProblemReport):
             return None
 
         return (my_session, session_start_time)
-
-    def get_timestamp(self):
-        '''Get timestamp (seconds since epoch) from Date field
-
-        Return None if it is not present.
-        '''
-        # report time is from asctime(), not in locale representation
-        orig_ctime = locale.getlocale(locale.LC_TIME)
-        try:
-            try:
-                locale.setlocale(locale.LC_TIME, 'C')
-                return time.mktime(time.strptime(self['Date']))
-            except KeyError:
-                return None
-            finally:
-                locale.setlocale(locale.LC_TIME, orig_ctime)
-        except locale.Error:
-            return None

problem_report.py
@@ -10,12 +10,11 @@
 # the full text of the license.
 
 import base64
-import datetime
 import gzip
+import locale
 import os
 import struct
 import time
-import typing
 import zlib
 from collections import UserDict
 from email.encoders import encode_base64
@@ -237,11 +236,23 @@ class ProblemReport(UserDict):
             raise ValueError('%s has no binary content' %
                              [item for item, element in b64_block.items() if element is False])
 
-    def get_date(self) -> typing.Optional[datetime.datetime]:
-        '''Return crash date as datetime object.'''
-        if 'Date' not in self:
+    def get_timestamp(self) -> int:
+        '''Get timestamp (seconds since epoch) from Date field
+
+        Return None if it is not present.
+        '''
+        # report time is from asctime(), not in locale representation
+        orig_ctime = locale.getlocale(locale.LC_TIME)
+        try:
+            try:
+                locale.setlocale(locale.LC_TIME, 'C')
+                return int(time.mktime(time.strptime(self['Date'])))
+            except KeyError:
+                return None
+            finally:
+                locale.setlocale(locale.LC_TIME, orig_ctime)
+        except locale.Error:
             return None
-        return datetime.datetime.strptime(self['Date'], '%a %b %d %H:%M:%S %Y')
 
     def has_removed_fields(self):
         '''Check if the report has any keys which were not loaded.

tests/unit/test_hookutils.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 import unittest.mock
 
@@ -45,8 +46,8 @@ class T(unittest.TestCase):
             [
                 "journalctl",
                 "--priority=warning",
-                "--since=2022-05-18 18:30:58",
-                "--until=2022-05-18 18:31:18",
+                f"--since=@{1652898658 + time.altzone}",
+                f"--until=@{1652898678 + time.altzone}",
             ],
         )
 

tests/unit/test_problem_report.py
@@ -1,4 +1,3 @@
-import datetime
 import email
 import locale
 import time
@@ -41,16 +40,32 @@ class T(unittest.TestCase):
         pr = problem_report.ProblemReport(date='19801224 12:34')
         self.assertEqual(pr['Date'], '19801224 12:34')
 
-    def test_get_date(self):
-        '''get_date() returns date.'''
-        pr = problem_report.ProblemReport(date='Wed May 18 09:49:57 2022')
-        self.assertEqual(pr.get_date(), datetime.datetime(2022, 5, 18, 9, 49, 57))
+    def test_get_timestamp(self):
+        '''get_timestamp() returns timestamp.'''
+        r = problem_report.ProblemReport()
+        self.assertAlmostEqual(r.get_timestamp(), time.time(), delta=2)
 
-    def test_get_date_returns_none(self):
-        '''get_date() returns None.'''
+        r['Date'] = 'Thu Jan 9 12:00:00 2014'
+        # delta is ±12 hours, as this depends on the timezone that the test is
+        # run in
+        self.assertAlmostEqual(r.get_timestamp(), 1389265200, delta=43200)
+
+    def test_get_timestamp_locale_german(self):
+        '''get_timestamp() returns date when LC_TIME is set.'''
+        pr = problem_report.ProblemReport(date='Wed May 18 09:49:57 2022')
+        orig_ctime = locale.getlocale(locale.LC_TIME)
+        try:
+            locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
+        except locale.Error:
+            self.skipTest("Missing German locale support")
+        self.assertEqual(pr.get_timestamp(), 1652867397 + time.altzone)
+        locale.setlocale(locale.LC_TIME, orig_ctime)
+
+    def test_get_timestamp_returns_none(self):
+        '''get_timestamp() returns None.'''
         pr = problem_report.ProblemReport()
         del pr['Date']
-        self.assertEqual(pr.get_date(), None)
+        self.assertEqual(pr.get_timestamp(), None)
 
     def test_sanity_checks(self):
         '''various error conditions.'''

tests/unit/test_report.py
@@ -1,5 +1,4 @@
 import os
-import time
 import unittest
 import unittest.mock
 
@@ -797,15 +796,3 @@ No symbol table info available.
         del pr['dmi.bios.version']
         expected = 'suspend/resume:Cray XT5'
         self.assertEqual(expected, pr.crash_signature())
-
-    def test_get_timestamp(self):
-        r = apport.Report()
-        self.assertAlmostEqual(r.get_timestamp(), time.time(), delta=2)
-
-        r['Date'] = 'Thu Jan 9 12:00:00 2014'
-        # delta is ±12 hours, as this depends on the timezone that the test is
-        # run in
-        self.assertAlmostEqual(r.get_timestamp(), 1389265200.0, delta=43200)
-
-        del r['Date']
-        self.assertEqual(r.get_timestamp(), None)