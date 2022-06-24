canonical/apport

commit 44d1c43231d70c2da9e51f3bd96ce8151902b354

Replace deprecated 'imp' module
The `imp` module is slated for removal in Python 3.12.

Bug: https://launchpad.net/bugs/1947425
Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

apport/report.py
@@ -14,7 +14,7 @@ import errno
 import fnmatch
 import glob
 import grp
-import imp
+import importlib
 import io
 import os
 import pwd
@@ -611,32 +611,16 @@ class Report(problem_report.ProblemReport):
 
         return None
 
-    @classmethod
-    def _python_module_path(klass, module):
+    @staticmethod
+    def _python_module_path(module):
         """Determine path of given Python module"""
-
-        module = module.replace("/", ".").split(".")
-        pathlist = sys.path
-
-        path = None
-        while module:
-            name = module.pop(0)
-
-            try:
-                (fd, path, desc) = imp.find_module(name, pathlist)
-            except ImportError:
-                path = None
-                break
-            if fd:
-                fd.close()
-            pathlist = [path]
-
-            if not module and desc[2] == imp.PKG_DIRECTORY:
-                module = ["__init__"]
-
-        if path and path.endswith(".pyc"):
-            path = path[:-1]
-        return path
+        try:
+            spec = importlib.util.find_spec(module)
+        except ImportError:
+            return None
+        if spec is None:
+            return None
+        return spec.origin
 
     def add_proc_info(self, pid=None, proc_pid_fd=None, extraenv=[]):
         """Add /proc/pid information.

tests/integration/test_backend_rpm.py
@@ -1,12 +1,16 @@
-import imp
 import shutil
 import unittest
+from importlib.machinery import SourceFileLoader
 
 from tests.paths import is_local_source_directory
 
 try:
     if is_local_source_directory():
-        impl = imp.load_source("", "backends/packaging_rpm.py").impl
+        impl = (
+            SourceFileLoader("", "backends/packaging_rpm.py")
+            .load_module()
+            .impl
+        )
     else:
         from apport.packaging_impl import impl
     HAS_RPM = True

tests/integration/test_report.py
@@ -489,10 +489,10 @@ class T(unittest.TestCase):
             pr = apport.report.Report()
             pr["ExecutablePath"] = "/usr/bin/python2.7"
             pr["ProcStatus"] = "Name:\tpython"
-            pr["ProcCmdline"] = "python\0-tt\0-m\0apport/report\0-v"
+            pr["ProcCmdline"] = "python\0-tt\0-m\0apport.report\0-v"
             pr._check_interpreted()
             self.assertEqual(pr["InterpreterPath"], "/usr/bin/python2.7")
-            self.assertIn("report", pr["ExecutablePath"])
+            self.assertIn("apport/report.py", pr["ExecutablePath"])
 
             # python script through -m, with dot separator; top-level module
             pr = apport.report.Report()
@@ -511,6 +511,26 @@ class T(unittest.TestCase):
             pr._check_interpreted()
             self.assertEqual(pr["InterpreterPath"], "/usr/bin/python3")
             self.assertIn("distutils/cmd.py", pr["ExecutablePath"])
+
+            # Python script through -m, non-existent module
+            pr = apport.report.Report()
+            pr["ExecutablePath"] = "/usr/bin/python3"
+            pr["ProcStatus"] = "Name:\tpython3"
+            pr["ProcCmdline"] = "python3\0-m\0nonexistent"
+            pr._check_interpreted()
+            self.assertNotIn("InterpreterPath", pr)
+            self.assertEqual(pr["ExecutablePath"], "/usr/bin/python3")
+            self.assertIn("UnreportableReason", pr)
+
+            # Python script through -m, non-existent sub-level module
+            pr = apport.report.Report()
+            pr["ExecutablePath"] = "/usr/bin/python3"
+            pr["ProcStatus"] = "Name:\tpython3"
+            pr["ProcCmdline"] = "python3\0-m\0apport.fileutils.nonexistent"
+            pr._check_interpreted()
+            self.assertNotIn("InterpreterPath", pr)
+            self.assertEqual(pr["ExecutablePath"], "/usr/bin/python3")
+            self.assertIn("UnreportableReason", pr)
         finally:
             if restore_root:
                 os.setresuid(0, 0, -1)

commit acaec25bbc4bf182674a0966b3525767263f25a2

Fix AttributeError: module 'importlib' has no attribute 'util'
On Ubuntu 22.10 (kinetic) some test cases fail during package build
with:

```
Traceback (most recent call last):
  File "data/recoverable_problem", line 84, in <module>
    main()
  File "data/recoverable_problem", line 44, in main
    report.add_proc_info(report.pid)
  File "apport/report.py", line 701, in add_proc_info
    self._check_interpreted()
  File "apport/report.py", line 541, in _check_interpreted
    path = self._python_module_path(cmdargs[2])
  File "report.py", line 618, in _python_module_path
    spec = importlib.util.find_spec(module)
AttributeError: module 'importlib' has no attribute 'util'
```

Bug: https://launchpad.net/bugs/1947425
Fixes: 44d1c43231d7 ("Replace deprecated 'imp' module")
Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

apport/report.py
@@ -14,7 +14,7 @@ import errno
 import fnmatch
 import glob
 import grp
-import importlib
+import importlib.util
 import io
 import os
 import pwd