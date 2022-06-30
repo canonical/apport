960f685bd730eb14363349cda3447b580c46a8a0
Benjamin Drung <bdrung@ubuntu.com>
2022-06-30 02:15:57 +0200

    tests: Ignore pylint false positives unexpected-keyword-arg
    Pylint complains about E1123: Unexpected keyword argument 'user' in
    constructor call (unexpected-keyword-arg), but `subprocess.Popen` takes
    a `user` keyword argument. See upstream bug
    https://github.com/PyCQA/pylint/issues/7092
    
    Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

--- tests/integration/test_signal_crashes.py
+++ tests/integration/test_signal_crashes.py
@@ -836,7 +836,8 @@ class T(unittest.TestCase):
         env = os.environ.copy()
         # set UTF-8 environment variable, to check proper parsing in apport
         os.putenv("utf8trap", b"\xc3\xa0\xc3\xa4")
-        process = subprocess.Popen(
+        # False positive, see https://github.com/PyCQA/pylint/issues/7092
+        process = subprocess.Popen(  # pylint: disable=unexpected-keyword-arg
             [command] + args, env=env, stdout=subprocess.DEVNULL, user=uid
         )
 
