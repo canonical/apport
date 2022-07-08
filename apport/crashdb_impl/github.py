cfeb80a5d3a78747e6c9929122b31027290a602e
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-07-08 13:25:03 +0200

    First commit

---
+++ apport/crashdb_impl/github.py
@@ -0,0 +1,170 @@
+"""Crash database implementation for Github."""
+
+# Copyright (C) 2022 - 2022 Canonical Ltd.
+# Author: Eduard Gómez Escanell <eduard.gomez.escandell@canonical.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+# the full text of the license.
+
+from dataclasses import dataclass
+import apport
+import apport.crashdb
+import requests
+import json
+import time
+
+
+class Github:
+    __last_request: float = time.time()
+
+    @staticmethod
+    def _stringify(data: dict) -> str:
+        "Takes a dict and returns it as a string"
+        string = ""
+        for key, value in data.items():
+            string = f"{string}&{key}={value}"
+        return string
+
+    def post(self, url: str, data: str):
+        headers = {
+            'Accept': 'application/vnd.github.v3+json'
+        }
+        if self.__access_token:
+            headers['Authorization'] = f'token {self.__access_token}'
+        result = requests.post(url, headers=headers, data=data)
+        self.__last_request = time.time()
+        result.raise_for_status()
+        return json.loads(result.text)
+
+    def api_authentication(self, url: str, data: dict):
+        return self.post(url, self._stringify(data))
+
+    def api_open_issue(self, owner: str, repo: str, data: dict):
+        url = f"https://api.github.com/repos/{owner}/{repo}/issues"
+        return self.post(url, json.dumps(data))
+
+    def __init__(self, client_id):
+        self.__client_id = client_id
+        self.__authentication_data = None
+        self.__access_token = None
+
+    def __enter__(self):
+        data = {
+            "client_id": self.__client_id,
+            "scope": "public_repo"
+        }
+        url = "https://github.com/login/device/code"
+        response = self.api_authentication(url, data)
+
+        # TODO: This should use UI!
+        print("Input the following code in your browser:")
+        print(f'URL:  {response["verification_uri"]}')
+        print(f'Code: {response["user_code"]}')
+
+        self.__authentication_data = {
+            "client_id": self.__client_id,
+            "device_code": f'{response["device_code"]}',
+            "grant_type": "urn:ietf:params:oauth:grant-type:device_code",
+        }
+        self.__cooldown = response["interval"]
+        self.__expiry = int(response["expires_in"]) + time.time()
+
+        return self
+
+    def __exit__(self, *_) -> None:
+        self.__authentication_data = None
+        self.__cooldown = 0
+        self.__expiry = 0
+
+    def authentication_complete(self) -> bool:
+        """
+        Asks Github if the user has introduced the code already.
+        It respects the wait-time requested by Github.
+        """
+        if not self.__authentication_data:
+            raise RuntimeError("Authentication not started. Use a with statement to do so")
+
+        t = time.time()
+        waittime = self.__cooldown - (t - self.__last_request)
+        if t + waittime > self.__expiry:
+            raise RuntimeError("Failed to log into Github: too much time elapsed.")
+        if waittime > 0:
+            time.sleep(waittime)
+
+        url = "https://github.com/login/oauth/access_token"
+        response = self.api_authentication(url, self.__authentication_data)
+
+        if "error" in response:
+            if response["error"] == "authorization_pending":
+                return False
+            if response["error"] == "slow_down":
+                self.__cooldown = int(response["interval"])
+                return False
+            raise RuntimeError(f"Unknown error from Github: {response}")
+        elif "access_token" in response:
+            self.__access_token = response["access_token"]
+            return True
+        raise RuntimeError(f"Unknown response from Github: {response}")
+
+@dataclass(frozen=True)
+class IssueHandle:
+    url: str
+
+class CrashDatabase(apport.crashdb.CrashDatabase):
+    """
+    Github crash database.
+    This is a Apport CrashDB implementation for interacting with Github issues
+    """
+
+    def __init__(self, auth_file, options):
+        """
+        Initialize some variables. Login is delayed until necessary.
+        """
+        apport.crashdb.CrashDatabase.__init__(self, auth_file, options)
+        self.repository_owner = options["repository_owner"]
+        self.repository_name = options["repository_name"]
+        self.app_id = options["github_app_id"]
+        self.labels = set(options["labels"])
+        self.issue_url = None
+
+    def _github_login(self) -> Github:
+        with Github(self.app_id) as github:
+            while not github.authentication_complete():
+                pass
+            return github
+
+    def _format_report(self, report: apport.Report) -> dict:
+        """
+        Formats report info as markdown and creates Github issue JSON.
+        """
+        body = ""
+        for key, value in report.items():
+            body += f"**{key}**\n{value}\n\n"
+
+        return {
+            "title": "Issue submitted via apport",
+            "body": body,
+            "labels": [l for l in self.labels]
+        }
+
+    def upload(self, report: apport.Report, progress_callback=None) -> IssueHandle:
+        """Upload given problem report return a handle for it.
+
+        In Github, we open an issue.
+        """
+        assert self.accepts(report)
+
+        data = self._format_report(report)
+        response = self._github_login().api_open_issue(self.repository_owner, self.repository_name, data)
+        return IssueHandle(url=response["html_url"])
+
+    def get_comment_url(self, report: apport.Report, handle: IssueHandle) -> str:
+        """
+        Return an URL that should be opened after report has been uploaded
+        and upload() returned handle.
+        """
+        return handle.url
+
