canonical/apport

commit fe7275c4c33ab4951fd28575f4f1ded1fca16f4b

Move obtaining the report information into a thread that's either called when the user presses the Show Details button or when they choose to report the bug.

apport/ui.py
@@ -145,6 +145,7 @@ class UserInterface:
         self.report = None
         self.report_file = None
         self.cur_package = None
+        self.collection_thread = None
 
         try:
             self.crashdb = get_crashdb(None)
@@ -181,6 +182,21 @@ class UserInterface:
 
         return result
 
+
+    def collect(self, callback=None):
+        def _go(callback=None):
+            self.collect_info()
+            import time
+            time.sleep(5)
+            if callable(callback):
+                callback()
+        self.collection_thread = apport.REThread.REThread(target=_go,
+                                                          args=(callback,))
+        self.collection_thread.start()
+
+    def touch_upload(self):
+        pass
+
     def run_crash(self, report_file, confirm=True):
         '''Present and report a particular crash.
 
@@ -213,53 +229,11 @@ class UserInterface:
                     _('unknown program')))
                 heading = _('Sorry, the program "%s" closed unexpectedly') % subject
                 self.ui_error_message(_('Problem in %s') % subject,
-                    '%s\n\n%s' % (heading, _('Your computer does not have enough \
-free memory to automatically analyze the problem and send a report to the developers.')))
+                    '%s\n\n%s' % (heading, _('Your computer does not have '
+                    'enough free memory to automatically analyze the problem '
+                    'and send a report to the developers.')))
                 return
 
-            # ask the user about what to do with the current crash
-            if not confirm:
-                pass
-            elif self.report.get('ProblemType') == 'Package':
-                response = self.ui_present_package_error()
-                if response == 'cancel':
-                    return
-                assert response == 'report'
-            elif self.report.get('ProblemType') == 'KernelCrash':
-                response = self.ui_present_kernel_error()
-                if response == 'cancel':
-                    return
-                assert response == 'report'
-            elif self.report.get('ProblemType') == 'KernelOops':
-                # XXX the string doesn't quite match this case
-                response = self.ui_present_kernel_error()
-                if response == 'cancel':
-                    return
-                assert response == 'report'
-            else:
-                try:
-                    desktop_entry = self.get_desktop_entry()
-                except ValueError: # package does not exist
-                    self.ui_error_message(_('Invalid problem report'),
-                        _('The report belongs to a package that is not installed.'))
-                    self.ui_shutdown()
-                    return
-
-                response = self.ui_present_crash(desktop_entry)
-                assert 'action' in response
-                assert 'blacklist' in response
-
-                if response['blacklist']:
-                    self.report.mark_ignore()
-
-                if response['action'] == 'cancel':
-                    return
-                if response['action'] == 'restart':
-                    self.restart()
-                    return
-                assert response['action'] == 'report'
-
-            # we want to file a bug now
             try:
                 if 'Dependencies' not in self.report:
                     self.collect_info()
@@ -277,27 +251,26 @@ free memory to automatically analyze the problem and send a report to the develo
                 self.ui_shutdown()
                 return
 
-            if self.check_unreportable():
+            desktop_file = self.get_desktop_entry()
+            response = self.ui_present_report_details(desktop_file)
+            if not self.collection_thread:
+                self.collect()
+            self.collection_thread.join()
+            self.collection_thread.exc_raise()
+            print 'response', response
+            if response['restart']:
+                self.restart()
+            if not response['report']:
                 return
 
+            self.touch_upload()
+            # We check for duplicates and unreportable crashes here, rather
+            # than before we show the dialog, as we want to submit these to the
+            # crash database, but not Launchpad.
             if self.handle_duplicate():
                 return
-
-            # confirm what will be sent
-            response = self.ui_present_report_details(False)
-            if response == 'cancel':
+            if self.check_unreportable():
                 return
-            if response == 'examine':
-                self.examine()
-                return
-            if response == 'reduced':
-                try:
-                    del self.report['CoreDump']
-                except KeyError:
-                    pass # Huh? Should not happen, but did in https://launchpad.net/bugs/86007
-            else:
-                assert response == 'full'
-
             self.file_report()
         except IOError as e:
             # fail gracefully if file is not readable for us
@@ -842,7 +815,9 @@ free memory to automatically analyze the problem and send a report to the develo
             cur_time = int(os.stat(self.report['ExecutablePath']).st_mtime)
 
             if orig_time != cur_time:
-                self.report['UnreportableReason'] = _('The problem happened with the program %s which changed since then.') % self.report['ExecutablePath']
+                self.report['UnreportableReason'] = (
+                    _('The problem happened with the program %s which changed '
+                      'since then.') % self.report['ExecutablePath'])
                 return
 
         if not self.cur_package and 'ExecutablePath' not in self.report \
@@ -851,79 +826,36 @@ free memory to automatically analyze the problem and send a report to the develo
             # package
             self.report.add_os_info()
         else:
-            # report might already be pre-processed by apport-retrace
             if self.report['ProblemType'] == 'Crash' and 'Stacktrace' in self.report:
                 return
-
-            # since this might take a while, create separate threads and
-            # display a progress dialog
-            self.ui_start_info_collection_progress()
 
             hookui = HookUI(self)
 
             if 'Stacktrace' not in self.report:
-                # save original environment, in case hooks change it
-                orig_env = os.environ.copy()
-
-                icthread = apport.REThread.REThread(target=thread_collect_info,
-                    name='thread_collect_info',
-                    args=(self.report, self.report_file, self.cur_package,
-                        hookui, symptom_script, ignore_uninstalled))
-                icthread.start()
-                while icthread.isAlive():
-                    self.ui_pulse_info_collection_progress()
-                    try:
-                        hookui.process_event()
-                    except KeyboardInterrupt:
-                        sys.exit(1)
-
-                icthread.join()
-
-                # restore original environment
-                os.environ.clear()
-                os.environ.update(orig_env)
-
-                icthread.exc_raise()
+                thread_collect_info(self.report, self.report_file, self.cur_package,
+                                    hookui, symptom_script, ignore_uninstalled)
 
             if 'CrashDB' in self.report:
                 self.crashdb = get_crashdb(None, self.report['CrashDB']) 
 
             # check bug patterns
-            if self.report['ProblemType'] == 'KernelCrash' or self.report['ProblemType'] == 'KernelOops' or 'Package' in self.report:
-                bpthread = apport.REThread.REThread(target=self.report.search_bug_patterns,
-                    args=(self.crashdb.get_bugpattern_baseurl(),))
-                bpthread.start()
-                while bpthread.isAlive():
-                    self.ui_pulse_info_collection_progress()
-                    try:
-                        bpthread.join(0.1)
-                    except KeyboardInterrupt:
-                        sys.exit(1)
-                bpthread.exc_raise()
-                if bpthread.return_value():
+            if (self.report['ProblemType'] == 'KernelCrash'
+                or self.report['ProblemType'] == 'KernelOops'
+                or 'Package' in self.report):
+                url = self.crashdb.get_bugpattern_baseurl() 
+                if self.report.search_bug_patterns(url):
                     self.report['KnownReport'] = bpthread.return_value()
 
             # check crash database if problem is known
             if self.report['ProblemType'] != 'Bug':
-                known_thread = apport.REThread.REThread(target=self.crashdb.known,
-                    args=(self.report,))
-                known_thread.start()
-                while known_thread.isAlive():
-                    self.ui_pulse_info_collection_progress()
-                    try:
-                        known_thread.join(0.1)
-                    except KeyboardInterrupt:
-                        sys.exit(1)
-                known_thread.exc_raise()
-                val = known_thread.return_value()
+                val = self.crashdb.known(self.report)
                 if val is not None:
                     self.report['KnownReport'] = val
 
-            self.ui_stop_info_collection_progress()
-
             # check that we were able to determine package names
-            if 'SourcePackage' not in self.report or \
-                (not self.report['ProblemType'].startswith('Kernel') and 'Package' not in self.report):
+            if ('SourcePackage' not in self.report or
+                (not self.report['ProblemType'].startswith('Kernel')
+                 and 'Package' not in self.report)):
                 self.ui_error_message(_('Invalid problem report'),
                     _('Could not determine the package or source package name.'))
                 # TODO This is not called consistently, is it really needed?

gtk/apport-gtk
@@ -25,6 +25,9 @@ except RuntimeError as e:
 import apport
 from apport import unicode_gettext as _
 import apport.ui
+from gi.repository import Gdk
+GObject.threads_init()
+Gdk.threads_init()
 
 class GTKUserInterface(apport.ui.UserInterface):
     '''GTK UserInterface.'''
@@ -61,6 +64,8 @@ class GTKUserInterface(apport.ui.UserInterface):
         self.str_heading_kernel_error = self.w('label_heading_kernel_error').get_label()
         self.str_radio_complete = self.w('radiobutton_complete').get_label()
         self.str_radio_reduced = self.w('radiobutton_reduced').get_label()
+        self.spinner = self.add_spinner_over_treeview(self.w('details_treeview'))
+        self.collect_called = False
 
     #
     # ui_* implementation of abstract UserInterface classes
@@ -147,8 +152,31 @@ class GTKUserInterface(apport.ui.UserInterface):
         else:
             return 'cancel'
 
-    def ui_present_report_details(self, desktop_file):
-        # report contents in expander
+    def add_spinner_over_treeview(self, treeview):
+        '''Reparents a treeview in a GtkOverlay, then layers a GtkSpinner
+        centered on top.'''
+        # TODO handle the expose event of the spinner so that we can draw on
+        # the treeview's viewport's window instead.
+        parent = treeview.get_parent()
+        parent.remove(treeview)
+        overlay = Gtk.Overlay()
+        parent.add_with_viewport(overlay)
+        overlay.add(treeview)
+        spinner = Gtk.Spinner()
+        spinner.set_size_request(42, 42)
+        align = Gtk.Alignment()
+        align.set_valign(Gtk.Align.CENTER)
+        align.set_halign(Gtk.Align.CENTER)
+        align.add(spinner)
+        overlay.add_overlay(align)
+        overlay.show()
+        align.show()
+        spinner.hide()
+        return spinner
+
+    def ui_update_view(self):
+        self.spinner.hide()
+        self.spinner.stop()
         self.tree_model.clear()
         for key in sorted(self.report):
             keyiter = self.tree_model.insert_before(None, None)
@@ -161,13 +189,26 @@ class GTKUserInterface(apport.ui.UserInterface):
                 self.tree_model.set_value(valiter, 0, self.report[key])
                 # expand the row if the value has less than 5 lines
                 if len(filter(lambda c: c == '\n', self.report[key])) < 4:
-                    self.w('details_treeview').expand_row(self.tree_model.get_path(keyiter), False)
+                    self.w('details_treeview').expand_row(
+                        self.tree_model.get_path(keyiter), False)
             else:
                 self.tree_model.set_value(valiter, 0, _('(binary data)'))
 
+    def ui_present_report_details(self, desktop_file):
+        icon = None
         if desktop_file:
-            from gi.repository import GdkPixbuf
             icon = desktop_file.getIcon()
+        report_type = self.report.get('ProblemType')
+        if report_type == 'KernelCrash' or report_type == 'KernelOops':
+            self.w('ignore_future_problems').set_active(False)
+            self.w('ignore_future_problems').hide()
+            self.w('title_label').set_label(
+                _('Ubuntu has restarted after experiencing an internal error.'))
+            self.w('subtitle_label').hide()
+            icon = 'distributor-logo'
+
+        if icon:
+            from gi.repository import GdkPixbuf
             builtin = Gtk.IconLookupFlags.USE_BUILTIN
             app_icon = self.w('application_icon')
             theme = Gtk.IconTheme.get_default()
@@ -193,6 +234,7 @@ class GTKUserInterface(apport.ui.UserInterface):
                 return_value['restart'] = True
             else:
                 return_value['restart'] = False
+            self.w('dialog_crash_new').hide()
             Gtk.main_quit()
 
         self.w('dialog_crash_new').connect('destroy', dialog_crash_dismissed)
@@ -225,22 +267,6 @@ class GTKUserInterface(apport.ui.UserInterface):
 
     def ui_error_message(self, title, text):
         self._ui_message_dialog(title, text, Gtk.MessageType.ERROR)
-
-    def ui_start_info_collection_progress(self):
-        self.w('progressbar_information_collection').set_fraction(0)
-        self.w('window_information_collection').show()
-        while Gtk.events_pending():
-            Gtk.main_iteration_do(False)
-
-    def ui_pulse_info_collection_progress(self):
-        self.w('progressbar_information_collection').pulse()
-        while Gtk.events_pending():
-            Gtk.main_iteration_do(False)
-
-    def ui_stop_info_collection_progress(self):
-        self.w('window_information_collection').hide()
-        while Gtk.events_pending():
-            Gtk.main_iteration_do(False)
 
     def ui_start_upload_progress(self):
         '''Open a window with an definite progress bar, telling the user to
@@ -376,6 +402,11 @@ class GTKUserInterface(apport.ui.UserInterface):
         else:
             sw.show()
             widget.set_label(_('Hide Details'))
+            if not self.collect_called:
+                self.spinner.show()
+                self.spinner.start()
+                self.collect(self.ui_update_view)
+                self.collect_called = True
         return True
 
     def on_progress_window_close_event(self, widget, event=None):

gtk/apport-gtk.ui
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <interface>
-  <requires lib="gtk+" version="2.16"/>
+  <requires lib="gtk+" version="3.0"/>
   <object class="GtkTreeStore" id="details_treestore">
     <columns>
       <!-- column-name gchararray1 -->

commit 80d952c2994d5d63eb4897cab96f3cc7218a966a

Remove unused confirm from run_crash
Fixes: fe7275c4c33a ("Move obtaining the report information into a thread[...]")
Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

apport/ui.py
@@ -322,7 +322,7 @@ class UserInterface:
 
         return result
 
-    def run_crash(self, report_file, confirm=True):
+    def run_crash(self, report_file):
         """Present and report a particular crash.
 
         If confirm is True, ask the user what to do about it, and offer to file
@@ -863,7 +863,7 @@ class UserInterface:
             return True
         elif self.args.crash_file:
             try:
-                self.run_crash(self.args.crash_file, False)
+                self.run_crash(self.args.crash_file)
             except OSError as error:
                 self.ui_error_message(_("Invalid problem report"), str(error))
             return True