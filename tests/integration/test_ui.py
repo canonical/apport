canonical/apport

commit f9214a85213d88ef4d6d88d3d79393c12b1d40bf

only setup a hookui if the crashdb will accept the report, this prevents hooks from asking questions but still allows them to collect information.

apport/ui.py
@@ -995,8 +995,12 @@ class UserInterface:
             # since this might take a while, create separate threads and
             # display a progress dialog.
             self.ui_start_info_collection_progress()
-
-            hookui = HookUI(self)
+            # only use a UI for asking questions if the crash db will accept
+            # the report
+            if self.crashdb.accepts(self.report):
+                hookui = HookUI(self)
+            else:
+                hookui = None
 
             if 'Stacktrace' not in self.report:
                 # save original environment, in case hooks change it
@@ -1008,10 +1012,11 @@ class UserInterface:
                 icthread.start()
                 while icthread.isAlive():
                     self.ui_pulse_info_collection_progress()
-                    try:
-                        hookui.process_event()
-                    except KeyboardInterrupt:
-                        sys.exit(1)
+                    if hookui:
+                        try:
+                            hookui.process_event()
+                        except KeyboardInterrupt:
+                            sys.exit(1)
 
                 icthread.join()
 

commit f8edc7280d492e710218c193e53542f1f86cfa91

Fix _run_hook getting called with ui=None
When totem crashes, `source_totem.py` will fail similar to the added
test case `test_hooks_choices_db_no_accept`:

```
ERROR: hook /tmp/tmpu6hm2b3x/package-hooks/coreutils.py crashed:
Traceback (most recent call last):
  File "apport/report.py", line 246, in _run_hook
    symb["add_info"](report, ui)
  File "/tmp/tmpu6hm2b3x/package-hooks/coreutils.py", line 3, in add_info
    answer = ui.choice('YourChoice?', ['foo', 'bar'])
AttributeError: 'NoneType' object has no attribute 'choice'
```

Steps to reproduce with totem:

```
totem &
killall -11 totem
ubuntu-bug /var/crash/_usr_bin_totem.1000.crash
```

In case the database does not accept reports, pass a HookUI instance to
`thread_collect_info` that behaves like a user that cancels all dialogs.

Bug: https://launchpad.net/bugs/1983481
Fixes: f9214a85213d ("only setup a hookui if the crashdb will accept the report[...]")
Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

apport/report.py
@@ -1073,6 +1073,7 @@ class Report(problem_report.ProblemReport):
         return True if the hook requested to stop the report filing process,
         False otherwise.
         """
+        assert ui is not None
         ret = self._add_hooks_info(ui, package, srcpackage)
         kill_pkttyagent()
         return ret

apport/ui.py
@@ -87,6 +87,7 @@ def thread_collect_info(
 
     If symptom_script is given, it will be run first (for run_symptom()).
     """
+    assert isinstance(ui, HookUI)
     try:
         report.add_gdb_info()
     except OSError:
@@ -1460,7 +1461,7 @@ class UserInterface:
             if self.crashdb.accepts(self.report):
                 hookui = HookUI(self)
             else:
-                hookui = None
+                hookui = NoninteractiveHookUI()
 
             if "Stacktrace" not in self.report:
                 # save original environment, in case hooks change it
@@ -1480,11 +1481,10 @@ class UserInterface:
                 icthread.start()
                 while icthread.is_alive():
                     self.ui_pulse_info_collection_progress()
-                    if hookui:
-                        try:
-                            hookui.process_event()
-                        except KeyboardInterrupt:
-                            sys.exit(1)
+                    try:
+                        hookui.process_event()
+                    except KeyboardInterrupt:
+                        sys.exit(1)
 
                 icthread.join()
 
@@ -2241,3 +2241,26 @@ class HookUI:
             *self._request_args
         )
         self._response_event.set()
+
+
+class NoninteractiveHookUI(HookUI):
+    """HookUI variant that does not ask the user any questions."""
+
+    def __init__(self):
+        super().__init__(None)
+
+    def information(self, text):
+        return None
+
+    def yesno(self, text):
+        return None
+
+    def choice(self, text, options, multiple=False):
+        return None
+
+    def file(self, text):
+        return None
+
+    def process_event(self):
+        # Give other threads some chance to run
+        time.sleep(0.1)

tests/integration/test_ui.py
@@ -2242,6 +2242,29 @@ class T(unittest.TestCase):
         self.assertEqual(self.ui.report["answer"], "None")
         self.assertEqual(self.ui.report["end"], "1")
 
+    def test_hooks_choices_db_no_accept(self):
+        """HookUI.choice() but DB does not accept report."""
+        self.ui.crashdb.accepts = lambda r: False
+        self.ui.present_details_response = {
+            "report": False,
+            "blacklist": False,
+            "examine": False,
+            "restart": False,
+            "remember": False,
+        }
+        self.ui.question_choice_response = [1]
+        self._run_hook(
+            textwrap.dedent(
+                """\
+                report['begin'] = '1'
+                answer = ui.choice('YourChoice?', ['foo', 'bar'])
+                report['answer'] = str(answer)
+                report['end'] = '1'
+                """
+            )
+        )
+        self.assertEqual(self.ui.report["answer"], "None")
+
     def test_interactive_hooks_cancel(self):
         """interactive hooks: user cancels"""
 