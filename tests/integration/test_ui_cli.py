canonical/apport

commit 9340a43dd934fe1150b6e395fbadb86939dcd353

Use subprocess.run instead of subprocess.Popen
`subprocess.run` is the recommended approach to invoking subprocesses
and it is easier to use.

Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

bin/apport-cli
@@ -182,10 +182,11 @@ class CLIUserInterface(apport.ui.UserInterface):
         self.in_update_view = True
         report = self._get_details()
         try:
-            p = subprocess.Popen(
-                ["/usr/bin/sensible-pager"], stdin=subprocess.PIPE
+            subprocess.run(
+                ["/usr/bin/sensible-pager"],
+                check=False,
+                stdin=report.encode("UTF-8"),
             )
-            p.communicate(report.encode("UTF-8"))
         except OSError as error:
             # ignore broken pipe (premature quit)
             if error.errno == errno.EPIPE:

apport/fileutils.py
@@ -384,21 +384,21 @@ def check_files_md5(sumfile):
     Return a list of files that don't match.
     """
     assert os.path.exists(sumfile)
-    m = subprocess.Popen(
+    md5sum = subprocess.run(
         ["/usr/bin/md5sum", "-c", sumfile],
+        check=False,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         cwd="/",
         env={},
     )
-    out = m.communicate()[0].decode()
 
     # if md5sum succeeded, don't bother parsing the output
-    if m.returncode == 0:
+    if md5sum.returncode == 0:
         return []
 
     mismatches = []
-    for line in out.splitlines():
+    for line in md5sum.stdout.decode().splitlines():
         if line.endswith("FAILED"):
             mismatches.append(line.rsplit(":", 1)[0])
 

apport/hookutils.py
@@ -439,19 +439,19 @@ def command_output(
     if not keep_locale:
         env["LC_MESSAGES"] = "C"
     try:
-        sp = subprocess.Popen(
+        sp = subprocess.run(
             command,
+            check=False,
+            input=input,
             stdout=subprocess.PIPE,
             stderr=stderr,
-            stdin=(input and subprocess.PIPE or None),
             env=env,
         )
     except OSError as error:
         return "Error: " + str(error)
 
-    out = sp.communicate(input)[0]
     if sp.returncode == 0:
-        res = out.strip()
+        res = sp.stdout.strip()
     else:
         res = (
             b"Error: command "
@@ -459,7 +459,7 @@ def command_output(
             + b" failed with exit code "
             + str(sp.returncode).encode()
             + b": "
-            + out
+            + sp.stdout
         )
 
     if decode_utf8:
@@ -586,10 +586,9 @@ def attach_root_command_outputs(report, command_map):
         script.close()
 
         # run script
-        sp = subprocess.Popen(
-            _root_command_prefix() + [wrapper_path, script_path]
+        subprocess.run(
+            _root_command_prefix() + [wrapper_path, script_path], check=False
         )
-        sp.wait()
 
         # now read back the individual outputs
         for keyname in command_map:
@@ -1042,17 +1041,17 @@ def _get_module_license(module):
     """Return the license for a given kernel module."""
 
     try:
-        modinfo = subprocess.Popen(
+        modinfo = subprocess.run(
             ["/sbin/modinfo", module],
+            check=False,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        out = modinfo.communicate()[0].decode("UTF-8")
         if modinfo.returncode != 0:
             return "invalid"
     except OSError:
         return None
-    for line in out.splitlines():
+    for line in modinfo.stdout.decode("UTF-8").splitlines():
         fields = line.split(":", 1)
         if len(fields) < 2:
             continue

apport/packaging.py
@@ -356,13 +356,13 @@ class PackageInfo:
                 )
 
         # fall back to lsb_release
-        p = subprocess.Popen(
+        lsb_release = subprocess.run(
             ["lsb_release", "-sir"],
+            check=False,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
+            text=True,
         )
-        (name, version) = (
-            p.communicate()[0].decode().strip().replace("\n", " ").split()
-        )
+        (name, version) = lsb_release.stdout.strip().replace("\n", " ").split()
         self._os_version = (name.strip(), version.strip())
         return self._os_version

apport/report.py
@@ -138,27 +138,28 @@ def _command_output(command, input=None, env=None):
     Try to execute given command (argv list) and return its stdout, or return
     a textual error if it failed.
     """
-    sp = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=env
-    )
-    # gdb can timeout when trying to retrace some core files, giving it 30
-    # minutes to run should be more than enough.
     try:
-        out = sp.communicate(input=input, timeout=1800)[0]
-    except subprocess.TimeoutExpired:
-        sp.kill()
-        out, errs = sp.communicate()
+        # gdb can timeout when trying to retrace some core files, giving it
+        # 30 minutes to run should be more than enough.
+        sp = subprocess.run(
+            command,
+            check=False,
+            env=env,
+            input=input,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            timeout=1800,
+        )
+    except subprocess.TimeoutExpired as error:
+        out = error.stdout.decode("UTF-8", errors="replace")
         raise OSError(
-            "Error: command %s timedout with exit code %i: %s"
-            % (str(command), sp.returncode, out)
+            f"Error: command {str(error.cmd)} timed out"
+            f" after {error.timeout} seconds: {out}"
         )
     if sp.returncode == 0:
-        return out
+        return sp.stdout
     else:
-        if out:
-            out = out.decode("UTF-8", errors="replace")
-        else:
-            out = ""
+        out = sp.stdout.decode("UTF-8", errors="replace")
         raise OSError(
             "Error: command %s failed with exit code %i: %s"
             % (str(command), sp.returncode, out)
@@ -825,23 +826,19 @@ class Report(problem_report.ProblemReport):
             kver = self["Uname"].split()[1]
             command = ["crash", "/usr/lib/debug/boot/vmlinux-%s" % kver, core]
             try:
-                p = subprocess.Popen(
+                crash = subprocess.run(
                     command,
-                    stdin=subprocess.PIPE,
+                    check=False,
+                    input=b"bt -a -f\nps\nrunq\nquit\n",
                     stdout=subprocess.PIPE,
                     stderr=subprocess.STDOUT,
                 )
             except OSError:
                 return False
-            p.stdin.write("bt -a -f\n")
-            p.stdin.write("ps\n")
-            p.stdin.write("runq\n")
-            p.stdin.write("quit\n")
-            # FIXME: split it up nicely etc
-            out = p.stdout.read()
-            ret = p.wait() == 0
+            ret = crash.returncode == 0
             if ret:
-                self["Stacktrace"] = out
+                # FIXME: split it up nicely etc
+                self["Stacktrace"] = crash.stdout
         finally:
             if unlink_core:
                 os.unlink(core)

apport/ui.py
@@ -890,15 +890,15 @@ class UserInterface:
                     " application window to report a problem about it."
                 ),
             )
-            xprop = subprocess.Popen(
+            xprop = subprocess.run(
                 ["xprop", "_NET_WM_PID"],
+                check=False,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
-            (out, err) = xprop.communicate()
             if xprop.returncode == 0:
                 try:
-                    self.options.pid = int(out.split()[-1])
+                    self.options.pid = int(xprop.stdout.split()[-1])
                 except ValueError:
                     self.ui_error_message(
                         _("Cannot create report"),
@@ -914,7 +914,7 @@ class UserInterface:
                     _("Cannot create report"),
                     _("xprop failed to determine process ID of the window")
                     + "\n\n"
-                    + err,
+                    + xprop.stderr.decode(),
                 )
                 return True
         else:

backends/packaging-apt-dpkg.py
@@ -464,17 +464,17 @@ class __AptDpkgPackageInfo(PackageInfo):
         official user-facing API for this, which will ask for confirmation and
         allows filtering.
         """
-        dpkg = subprocess.Popen(
+        dpkg = subprocess.run(
             ["dpkg-query", "-W", "--showformat=${Conffiles}", "--", package],
+            check=False,
             stdout=subprocess.PIPE,
         )
 
-        out = dpkg.communicate()[0].decode()
         if dpkg.returncode != 0:
             return {}
 
         modified = {}
-        for line in out.splitlines():
+        for line in dpkg.stdout.decode().splitlines():
             if not line:
                 continue
             # just take the first two fields, to not stumble over obsolete
@@ -507,16 +507,16 @@ class __AptDpkgPackageInfo(PackageInfo):
         i = 0
 
         while not match and i < len(file_list):
-            p = subprocess.Popen(
+            fgrep = subprocess.run(
                 ["fgrep", "-lxm", "1", "--", pattern]
                 + file_list[i : (i + slice_size)],
+                check=False,
                 stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
-            out = p.communicate()[0].decode("UTF-8")
-            if p.returncode == 0:
-                match = out
+            if fgrep.returncode == 0:
+                match = fgrep.stdout.decode("UTF-8")
             i += slice_size
 
         return match
@@ -545,12 +545,13 @@ class __AptDpkgPackageInfo(PackageInfo):
             return self._search_contents(file, map_cachedir, release, arch)
 
         # check if the file is a diversion
-        dpkg = subprocess.Popen(
+        dpkg = subprocess.run(
             ["dpkg-divert", "--list", file],
+            check=False,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        out = dpkg.communicate()[0].decode("UTF-8")
+        out = dpkg.stdout.decode("UTF-8")
         if dpkg.returncode == 0 and out:
             pkg = out.split()[-1]
             if pkg != "hardening-wrapper":
@@ -589,11 +590,12 @@ class __AptDpkgPackageInfo(PackageInfo):
         """Return the architecture of the system, in the notation used by the
         particular distribution."""
 
-        dpkg = subprocess.Popen(
-            ["dpkg", "--print-architecture"], stdout=subprocess.PIPE
+        dpkg = subprocess.run(
+            ["dpkg", "--print-architecture"],
+            check=True,
+            stdout=subprocess.PIPE,
         )
-        arch = dpkg.communicate()[0].decode().strip()
-        assert dpkg.returncode == 0
+        arch = dpkg.stdout.decode().strip()
         assert arch
         return arch
 
@@ -604,12 +606,12 @@ class __AptDpkgPackageInfo(PackageInfo):
         $LD_LIBRARY_PATH. This needs to take any multiarch directories into
         account.
         """
-        dpkg = subprocess.Popen(
+        dpkg = subprocess.run(
             ["dpkg-architecture", "-qDEB_HOST_MULTIARCH"],
+            check=True,
             stdout=subprocess.PIPE,
         )
-        multiarch_triple = dpkg.communicate()[0].decode().strip()
-        assert dpkg.returncode == 0
+        multiarch_triple = dpkg.stdout.decode().strip()
 
         return "/lib/%s:/lib" % multiarch_triple
 
@@ -1367,12 +1369,14 @@ class __AptDpkgPackageInfo(PackageInfo):
         """Call dpkg with given arguments and return output, or return None on
         error."""
 
-        dpkg = subprocess.Popen(
-            ["dpkg"] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        dpkg = subprocess.run(
+            ["dpkg"] + args,
+            check=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
         )
-        out = dpkg.communicate(input)[0].decode("UTF-8")
         if dpkg.returncode == 0:
-            return out
+            return dpkg.stdout.decode("UTF-8")
         else:
             raise ValueError("package does not exist")
 
@@ -1383,31 +1387,28 @@ class __AptDpkgPackageInfo(PackageInfo):
         testable.
         """
         if os.path.exists(sumfile):
-            m = subprocess.Popen(
-                ["/usr/bin/md5sum", "-c", sumfile],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                cwd="/",
-                env={},
-            )
-            out = m.communicate()[0].decode("UTF-8", errors="replace")
+            args = [sumfile]
+            stdin = None
         else:
             assert (
                 type(sumfile) == bytes
             ), "md5sum list value must be a byte array"
-            m = subprocess.Popen(
-                ["/usr/bin/md5sum", "-c"],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                cwd="/",
-                env={},
-            )
-            out = m.communicate(sumfile)[0].decode("UTF-8", errors="replace")
+            args = []
+            stdin = sumfile
+        md5sum = subprocess.run(
+            ["/usr/bin/md5sum", "-c"] + args,
+            check=False,
+            input=stdin,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd="/",
+            env={},
+        )
 
         # if md5sum succeeded, don't bother parsing the output
-        if m.returncode == 0:
+        if md5sum.returncode == 0:
             return []
+        out = md5sum.stdout.decode("UTF-8", errors="replace")
 
         mismatches = []
         for line in out.splitlines():
@@ -1869,11 +1870,12 @@ class __AptDpkgPackageInfo(PackageInfo):
     def _deb_version(klass, pkg):
         """Return the version of a .deb file"""
 
-        dpkg = subprocess.Popen(
-            ["dpkg-deb", "-f", pkg, "Version"], stdout=subprocess.PIPE
+        dpkg = subprocess.run(
+            ["dpkg-deb", "-f", pkg, "Version"],
+            check=True,
+            stdout=subprocess.PIPE,
         )
-        out = dpkg.communicate(input)[0].decode("UTF-8").strip()
-        assert dpkg.returncode == 0
+        out = dpkg.stdout.decode("UTF-8").strip()
         assert out
         return out
 
@@ -1891,13 +1893,10 @@ class __AptDpkgPackageInfo(PackageInfo):
         """Get "lsb_release -sc", cache the result."""
 
         if self._distro_codename is None:
-            lsb_release = subprocess.Popen(
-                ["lsb_release", "-sc"], stdout=subprocess.PIPE
+            lsb_release = subprocess.run(
+                ["lsb_release", "-sc"], check=True, stdout=subprocess.PIPE
             )
-            self._distro_codename = (
-                lsb_release.communicate()[0].decode("UTF-8").strip()
-            )
-            assert lsb_release.returncode == 0
+            self._distro_codename = lsb_release.stdout.decode("UTF-8").strip()
 
         return self._distro_codename
 

backends/packaging_rpm.py
@@ -166,10 +166,13 @@ class RPMPackageInfo:
     def get_system_architecture(self):
         """Return the architecture of the system, in the notation used by the
         particular distribution."""
-        rpmarch = subprocess.Popen(
-            ["rpm", "--eval", "%_target_cpu"], stdout=subprocess.PIPE
+        rpmarch = subprocess.run(
+            ["rpm", "--eval", "%_target_cpu"],
+            check=True,
+            stdout=subprocess.PIPE,
+            text=True,
         )
-        arch = rpmarch.communicate()[0].strip()
+        arch = rpmarch.stdout.strip()
         return arch
 
     def is_distro_package(self, package):

commit 958c31ced48aeebbce858cef52e5b75ff6faa0b9

apport-cli: Fix AttributeError: 'bytes' object has no attribute 'fileno'
apport-cli fails when viewing the crash report:

```
Traceback (most recent call last):
  File "bin/apport-cli", line 187, in ui_update_view
    subprocess.run(
  File "/usr/lib/python3.10/subprocess.py", line 501, in run
    with Popen(*popenargs, **kwargs) as process:
  File "/usr/lib/python3.10/subprocess.py", line 835, in __init__
    errread, errwrite) = self._get_handles(stdin, stdout, stderr)
  File "/usr/lib/python3.10/subprocess.py", line 1606, in _get_handles
    p2cread = stdin.fileno()
AttributeError: 'bytes' object has no attribute 'fileno'
```

The `stdin` parameter for `subprocess.run` must be an existing file
descriptor or an existing file object with a valid file descriptor. Use
the `input` for byte sequences.

Bug: https://launchpad.net/bugs/1991200
Fixes: 9340a43dd934 ("Use subprocess.run instead of subprocess.Popen")
Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

bin/apport-cli
@@ -180,14 +180,15 @@ class CLIUserInterface(apport.ui.UserInterface):
 
         return details
 
-    def ui_update_view(self):
+    def ui_update_view(self, stdout=None):
         self.in_update_view = True
         report = self._get_details()
         try:
             subprocess.run(
                 ["/usr/bin/sensible-pager"],
                 check=False,
-                stdin=report.encode("UTF-8"),
+                input=report.encode("UTF-8"),
+                stdout=stdout,
             )
         except OSError as error:
             # ignore broken pipe (premature quit)

tests/integration/test_ui_cli.py
@@ -1,0 +1,69 @@
+# Copyright (C) 2022 Canonical Ltd.
+# Author: Benjamin Drung <benjamin.drung@canonical.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+# the full text of the license.
+
+"""Command line Apport user interface tests."""
+
+import os
+import sys
+import unittest
+
+import apport.report
+from tests.helper import import_module_from_file, skip_if_command_is_missing
+from tests.paths import is_local_source_directory, local_test_environment
+
+if is_local_source_directory():
+    apport_cli_path = "bin/apport-cli"
+else:
+    apport_cli_path = os.path.join(
+        os.environ.get("APPORT_DATA_DIR", "/usr/share/apport"), "apport-cli"
+    )
+apport_cli = import_module_from_file(apport_cli_path)
+
+
+class TestApportCli(unittest.TestCase):
+    """Test apport-cli."""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.orig_environ = os.environ.copy()
+        os.environ |= local_test_environment()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.environ.clear()
+        os.environ.update(cls.orig_environ)
+
+    def setUp(self):
+        saved = sys.argv
+        sys.argv = [apport_cli_path]
+        self.app = apport_cli.CLIUserInterface()
+        sys.argv = saved
+
+        self.app.report = apport.report.Report()
+        self.app.report.add_os_info()
+        self.app.report["ExecutablePath"] = "/bin/bash"
+        self.app.report["Signal"] = "11"
+        self.app.report["CoreDump"] = b"\x01\x02"
+
+    @skip_if_command_is_missing("/usr/bin/sensible-pager")
+    def test_ui_update_view(self):
+        read_fd, write_fd = os.pipe()
+        with os.fdopen(write_fd, "w", buffering=1) as stdout:
+            self.app.ui_update_view(stdout=stdout)
+        with os.fdopen(read_fd, "r") as pipe:
+            report = pipe.read()
+        self.assertIn(
+            "== ExecutablePath =================================\n"
+            "/bin/bash\n\n"
+            "== ProblemType =================================\n"
+            "Crash\n\n"
+            "== Signal =================================\n"
+            "11\n\n",
+            report,
+        )