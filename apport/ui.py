4701039b12ee3d395f273858011946dce3da067a
Benjamin Drung <benjamin.drung@canonical.com>
2022-10-05 10:50:44 +0200

    Release apport 2.23.1
    Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

--- apport/ui.py
+++ apport/ui.py
@@ -44,7 +44,7 @@ import apport.REThread
 from apport import unicode_gettext as _
 from apport.packaging_impl import impl as packaging
 
-__version__ = "2.23.0"
+__version__ = "2.23.1"
 
 
 symptom_script_dir = os.environ.get(
