canonical/apport

commit 5a504336b2f6bafd82f268c5dc5e9a4db4cc9bcd

refactor: Use f-strings everywhere
Use [flynt](https://github.com/ikamensh/flynt) to convert the code from
old `%`-formatted and `.format(...)` strings into Python 3.6+'s
"f-strings". Not all instances were converted by flynt. The remaining
ones were converted manually.

Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

apport/hookutils.py
@@ -107,7 +107,7 @@ def read_file(path, force_unicode=False):
         except UnicodeDecodeError:
             return contents
     except OSError as error:
-        return "Error: " + str(error)
+        return f"Error: {str(error)}"
 
 
 def attach_file(report, path, key=None, overwrite=True, force_unicode=False):
@@ -145,7 +145,7 @@ def attach_conffiles(report, package, conffiles=None, ui=None):
         if conffiles and path not in conffiles:
             continue
 
-        key = "modified.conffile." + path_to_key(path)
+        key = f"modified.conffile.{path_to_key(path)}"
         if isinstance(contents, str) and (
             contents == "[deleted]" or contents.startswith("[inaccessible")
         ):
@@ -154,9 +154,8 @@ def attach_conffiles(report, package, conffiles=None, ui=None):
 
         if ui:
             response = ui.yesno(
-                'It seems you have modified the contents of "%s".  '
-                "Would you like to add the contents of it to your bug report?"
-                % path
+                f'It seems you have modified the contents of "{path}".  '
+                f"Would you like to add the contents of it to your bug report?"
             )
             if response:
                 report[key] = contents
@@ -166,7 +165,7 @@ def attach_conffiles(report, package, conffiles=None, ui=None):
             report[key] = "[modified]"
 
         mtime = datetime.datetime.fromtimestamp(os.stat(path).st_mtime)
-        report["mtime.conffile." + path_to_key(path)] = mtime.isoformat()
+        report[f"mtime.conffile.{path_to_key(path)}"] = mtime.isoformat()
 
 
 def attach_upstart_overrides(report, package):
@@ -179,7 +178,7 @@ def attach_upstart_overrides(report, package):
     for file in files:
         if os.path.exists(file) and file.startswith("/etc/init/"):
             override = file.replace(".conf", ".override")
-            key = "upstart." + override.replace("/etc/init/", "")
+            key = f"upstart.{override.replace('/etc/init/', '')}"
             attach_file_if_exists(report, override, key)
 
 
@@ -195,7 +194,7 @@ def attach_upstart_logs(report, package):
             continue
         if f.startswith("/usr/share/upstart/sessions/"):
             log = os.path.basename(f).replace(".conf", ".log")
-            key = "upstart." + log
+            key = f"upstart.{log}"
             try:
                 log = os.path.join(
                     os.environ["XDG_CACHE_HOME"], "upstart", log
@@ -212,8 +211,8 @@ def attach_upstart_logs(report, package):
 
         if f.startswith("/usr/share/applications/") and f.endswith(".desktop"):
             desktopname = os.path.splitext(os.path.basename(f))[0]
-            key = "upstart.application." + desktopname
-            log = "application-%s.log" % desktopname
+            key = f"upstart.application.{desktopname}"
+            log = f"application-{desktopname}.log"
             try:
                 log = os.path.join(
                     os.environ["XDG_CACHE_HOME"], "upstart", log
@@ -242,7 +241,7 @@ def attach_dmi(report):
     dmi_dir = "/sys/class/dmi/id"
     if os.path.isdir(dmi_dir):
         for f in os.listdir(dmi_dir):
-            p = os.path.realpath("%s/%s" % (dmi_dir, f))
+            p = os.path.realpath(f"{dmi_dir}/{f}")
             st = os.stat(p)
             # ignore the root-only ones, since they have serial numbers
             if not stat.S_ISREG(st.st_mode) or (st.st_mode & 4 == 0):
@@ -255,7 +254,7 @@ def attach_dmi(report):
             except OSError:
                 continue
             if value:
-                report["dmi." + f.replace("_", ".")] = value
+                report[f"dmi.{f.replace('_', '.')}"] = value
 
 
 def attach_hardware(report):
@@ -308,10 +307,9 @@ def attach_hardware(report):
 
     # Use the hardware information to create a machine type.
     if "dmi.sys.vendor" in report and "dmi.product.name" in report:
-        report["MachineType"] = "%s %s" % (
-            report["dmi.sys.vendor"],
-            report["dmi.product.name"],
-        )
+        report[
+            "MachineType"
+        ] = "{report['dmi.sys.vendor']} {report['dmi.product.name']}"
 
     if command_available("prtconf"):
         report["Prtconf"] = command_output(["prtconf"])
@@ -357,24 +355,23 @@ def attach_alsa_old(report):
                     cards.append(int(fields[0]))
 
     for card in cards:
-        key = "Card%d.Amixer.info" % card
+        key = f"Card{card}.Amixer.info"
         report[key] = command_output(["amixer", "-c", str(card), "info"])
-        key = "Card%d.Amixer.values" % card
+        key = f"Card{card}.Amixer.values"
         report[key] = command_output(["amixer", "-c", str(card)])
 
-        for codecpath in glob.glob("/proc/asound/card%d/codec*" % card):
+        for codecpath in glob.glob(f"/proc/asound/card{card}/codec*"):
             if os.path.isfile(codecpath):
                 codec = os.path.basename(codecpath)
-                key = "Card%d.Codecs.%s" % (card, path_to_key(codec))
+                key = f"Card{card}.Codecs.{path_to_key(codec)}"
                 attach_file(report, codecpath, key=key)
             elif os.path.isdir(codecpath):
                 codec = os.path.basename(codecpath)
                 for name in os.listdir(codecpath):
                     path = os.path.join(codecpath, name)
-                    key = "Card%d.Codecs.%s.%s" % (
-                        card,
-                        path_to_key(codec),
-                        path_to_key(name),
+                    key = (
+                        f"Card{card}.Codecs"
+                        f".{path_to_key(codec)}.{path_to_key(name)}"
                     )
                     attach_file(report, path, key)
 
@@ -451,7 +448,7 @@ def command_output(
             env=env,
         )
     except OSError as error:
-        return "Error: " + str(error)
+        return f"Error: {str(error)}"
 
     if sp.returncode == 0:
         res = sp.stdout.strip()
@@ -583,8 +580,7 @@ def attach_root_command_outputs(report, command_map):
                 # use "| cat" here, so that we can end commands with 2>&1
                 # (otherwise it would have the wrong redirection order)
                 script.write(
-                    "%s | cat > %s\n"
-                    % (command, os.path.join(workdir, keyname))
+                    f"{command} | cat > {os.path.join(workdir, keyname)}\n"
                 )
 
         # run script
@@ -790,7 +786,7 @@ def attach_gsettings_schema(report, schema):
                     b"favorite-apps",
                 ]:
                     value = "redacted by apport"
-                cur_value += "%s %s %s\n" % (schema_name, key, value)
+                cur_value += f"{schema_name} {key} {value}\n"
 
     report["GsettingsChanges"] = cur_value
 
@@ -947,7 +943,7 @@ def attach_mac_events(report, profiles=None):
     if "AuditLog" not in report and os.path.exists("/var/run/auditd.pid"):
         attach_root_command_outputs(
             report,
-            {"AuditLog": 'egrep "' + mac_regex + '" /var/log/audit/audit.log'},
+            {"AuditLog": f'egrep "{mac_regex}" /var/log/audit/audit.log'},
         )
 
     attach_file_if_exists(
@@ -973,7 +969,7 @@ def attach_mac_events(report, profiles=None):
             continue
 
         for search_profile in profiles:
-            if re.match("^" + search_profile + "$", profile):
+            if re.match(f"^{search_profile}$", profile):
                 report.add_tags(["apparmor"])
                 break
 
@@ -1013,8 +1009,7 @@ def package_versions(*packages):
             versions.append((package, version))
 
     package_width = max(len(version[0]) for version in versions)
-    fmt = "%%-%ds %%s" % package_width
-    return "\n".join([fmt % v for v in versions])
+    return "\n".join([f"{p:<{package_width}s} {v}" for (p, v) in versions])
 
 
 def _get_module_license(module):
@@ -1071,7 +1066,7 @@ def __drm_con_info(con):
         if f == "edid":
             val = base64.b64encode(val)
             f += "-base64"
-        info += "%s: %s\n" % (f, val.decode("UTF-8", errors="replace"))
+        info += f"{f}: {val.decode('UTF-8', errors='replace')}\n"
     return info
 
 
@@ -1087,7 +1082,7 @@ def attach_drm_info(report):
         con = os.path.join(drm_dir, f)
         if os.path.exists(os.path.join(con, "enabled")):
             # DRM can set an arbitrary string for its connector paths.
-            report["DRM." + path_to_key(f)] = __drm_con_info(con)
+            report[f"DRM.{path_to_key(f)}"] = __drm_con_info(con)
 
 
 def in_session_of_problem(report):
@@ -1124,7 +1119,7 @@ def in_session_of_problem(report):
     # determine session creation time
     try:
         session_start_time = os.stat(
-            "/run/systemd/sessions/" + session_id
+            f"/run/systemd/sessions/{session_id}"
         ).st_mtime
     except OSError:
         return None

tests/integration/test_hookutils.py
@@ -32,7 +32,7 @@ class T(unittest.TestCase):
         def _build_ko(license_name):
             ko_filename = os.path.join(self.workdir, f"{license_name}.ko")
             with tempfile.NamedTemporaryFile(
-                mode="w+", prefix="%s-" % (license_name), suffix=".S"
+                mode="w+", prefix=f"{license_name}-", suffix=".S"
             ) as asm:
                 asm.write(
                     f'.section .modinfo\n.string "license={license_name}"\n'
@@ -381,7 +381,7 @@ class T(unittest.TestCase):
         data = apport.hookutils.recent_syslog(re.compile("kernel"), path=log)
         mem_after = self._get_mem_usage()
         delta_kb = mem_after - mem_before
-        sys.stderr.write("[Δ %i kB] " % delta_kb)
+        sys.stderr.write(f"[Δ {delta_kb} kB] ")
         self.assertLess(delta_kb, 5000)
 
         self.assertTrue(

.pylintrc
@@ -29,7 +29,6 @@ good-names=i,j,k,lp,ui,_
 # --disable=W".
 disable=bad-option-value,fixme,
   # TODO: Fix all following disabled checks!
-  consider-using-f-string,
   duplicate-code,
   missing-docstring,
 

apport/crashdb.py
@@ -123,7 +123,7 @@ class CrashDatabase:
         cur.execute("PRAGMA integrity_check")
         result = cur.fetchall()
         if result != [("ok",)]:
-            raise SystemError("Corrupt duplicate db:" + str(result))
+            raise SystemError(f"Corrupt duplicate db:{result}")
 
         try:
             cur.execute("SELECT format FROM version")
@@ -133,11 +133,11 @@ class CrashDatabase:
                 # first db format did not have version table yet
                 result = [0]
         if result[0] > self.format_version:
-            raise SystemError("duplicate DB has unknown format %i" % result[0])
+            raise SystemError(f"duplicate DB has unknown format {result[0]}")
         if result[0] < self.format_version:
             print(
-                "duplicate db has format %i, upgrading to %i"
-                % (result[0], self.format_version)
+                f"duplicate db has format {result[0]},"
+                f" upgrading to {self.format_version}"
             )
             self._duplicate_db_upgrade(result[0])
 
@@ -413,7 +413,7 @@ class CrashDatabase:
 
         # first create the temporary new dir; if that fails, nothing has been
         # changed and we fail early
-        out = publish_dir + ".new"
+        out = f"{publish_dir}.new"
         os.mkdir(out)
 
         # crash addresses
@@ -438,7 +438,7 @@ class CrashDatabase:
                     os.path.join(addr_base, cur_hash), "w", encoding="utf-8"
                 )
 
-            cur_file.write("%i %s\n" % (crash_id, sig))
+            cur_file.write(f"{crash_id} {sig}\n")
 
         if cur_file:
             cur_file.close()
@@ -463,17 +463,17 @@ class CrashDatabase:
                     cur_file.close()
                 cur_file = open(os.path.join(sig_base, cur_hash), "wb")
 
-            cur_file.write(("%i %s\n" % (crash_id, sig)).encode("UTF-8"))
+            cur_file.write(f"{crash_id} {sig}\n".encode("UTF-8"))
 
         if cur_file:
             cur_file.close()
 
         # switch over tree; this is as atomic as we can be with directories
         if os.path.exists(publish_dir):
-            os.rename(publish_dir, publish_dir + ".old")
+            os.rename(publish_dir, f"{publish_dir}.old")
         os.rename(out, publish_dir)
-        if os.path.exists(publish_dir + ".old"):
-            shutil.rmtree(publish_dir + ".old")
+        if os.path.exists(f"{publish_dir}.old"):
+            shutil.rmtree(f"{publish_dir}.old")
 
     def _duplicate_db_upgrade(self, cur_format):
         """Upgrade database to current format."""
@@ -598,8 +598,8 @@ class CrashDatabase:
         # crash got rejected
         if real_fixed_version == "invalid":
             print(
-                "DEBUG: bug %i was invalidated, removing from database"
-                % crash_id
+                f"DEBUG: bug {crash_id} was invalidated,"
+                f" removing from database"
             )
             self.duplicate_db_remove(crash_id)
             return
@@ -607,8 +607,8 @@ class CrashDatabase:
         # crash got fixed
         if not db_fixed_version and real_fixed_version:
             print(
-                "DEBUG: bug %i got fixed in version %s, updating database"
-                % (crash_id, real_fixed_version)
+                f"DEBUG: bug {crash_id} got fixed"
+                f" in version {real_fixed_version}, updating database"
             )
             self.duplicate_db_fixed(crash_id, real_fixed_version)
             return
@@ -616,9 +616,8 @@ class CrashDatabase:
         # crash got reopened
         if db_fixed_version and not real_fixed_version:
             print(
-                "DEBUG: bug %i got reopened,"
-                " dropping fixed version %s from database"
-                % (crash_id, db_fixed_version)
+                f"DEBUG: bug {crash_id} got reopened,"
+                f" dropping fixed version {db_fixed_version} from database"
             )
             self.duplicate_db_fixed(crash_id, real_fixed_version)
             return
@@ -629,9 +628,8 @@ class CrashDatabase:
         if existing:
             if existing != crash_id:
                 raise SystemError(
-                    "ID %i has signature %s, but database"
-                    " already has that signature for ID %i"
-                    % (crash_id, sig, existing)
+                    f"ID {crash_id} has signature {sig}, but database"
+                    f" already has that signature for ID {existing}"
                 )
         else:
             cur = self.duplicate_db.cursor()
@@ -947,7 +945,7 @@ def get_crashdb(auth_file, name=None, conf=None):
         exec(compile(f.read(), conf, "exec"), settings)
 
     # Load third parties crashdb.conf
-    confdDir = conf + ".d"
+    confdDir = f"{conf}.d"
     if os.path.isdir(confdDir):
         for cf in os.listdir(confdDir):
             cfpath = os.path.join(confdDir, cf)
@@ -961,9 +959,7 @@ def get_crashdb(auth_file, name=None, conf=None):
                         )
                 except Exception as error:  # pylint: disable=broad-except
                     # ignore broken files
-                    sys.stderr.write(
-                        "Invalid file %s: %s\n" % (cfpath, str(error))
-                    )
+                    sys.stderr.write(f"Invalid file {cfpath}: {error}\n")
 
     if not name:
         name = settings["default"]
@@ -977,7 +973,7 @@ def load_crashdb(auth_file, spec):
     spec is a crash db configuration dictionary as described in get_crashdb().
     """
     m = __import__(
-        "apport.crashdb_impl." + spec["impl"],
+        f"apport.crashdb_impl.{spec['impl']}",
         globals(),
         locals(),
         ["CrashDatabase"],

apport/crashdb_impl/debian.py
@@ -80,12 +80,8 @@ class CrashDatabase(apport.crashdb.CrashDatabase):
             return False
 
         with tempfile.NamedTemporaryFile() as temp:
-            temp.file.write(
-                ("Package: " + buggy_package + "\n").encode("UTF-8")
-            )
-            temp.file.write(
-                ("Version: " + buggy_version + "\n\n\n").encode("UTF-8")
-            )
+            temp.file.write(f"Package: {buggy_package}\n".encode("UTF-8"))
+            temp.file.write(f"Version: {buggy_version}\n\n\n".encode("UTF-8"))
             temp.file.write(
                 ("=============================\n\n").encode("UTF-8")
             )
@@ -114,7 +110,7 @@ class CrashDatabase(apport.crashdb.CrashDatabase):
 
         # Subscribe the submitted to the bug report
         msg.add_header("X-Debbugs-CC", self.options["sender"])
-        msg.add_header("Usertag", "apport-%s" % report["ProblemType"].lower())
+        msg.add_header("Usertag", f"apport-{report['ProblemType'].lower()}")
 
         smtp = smtplib.SMTP(self.options["smtphost"])
         smtp.sendmail(

commit f303ea423ecc80d943abb52af6c9d5ae8c3a646b

fix: MachineType f-string interpolation
It looks like when things were changed over to f-strings this one line missed
the "f". I did some basic searches through the rest of the code to find other
instances of this but couldn't find any. Moved to attach_dmi since it's
derived from values there.

fixes: 5a504336b2f6bafd82f268c5dc5e9a4db4cc9bcd ("refactor: Use f-strings everywhere")

Signed-off-by: Chris Peterson <chris.peterson@canonical.com>

apport/hookutils.py
@@ -219,6 +219,12 @@ def attach_dmi(report):
             if value:
                 report[f"dmi.{f.replace('_', '.')}"] = value
 
+    # Use the hardware information to create a machine type.
+    if "dmi.sys.vendor" in report and "dmi.product.name" in report:
+        report[
+            "MachineType"
+        ] = f"{report['dmi.sys.vendor']} {report['dmi.product.name']}"
+
 
 def attach_hardware(report):
     """Attach a standard set of hardware-related data to the report, including:
@@ -265,12 +271,6 @@ def attach_hardware(report):
     report["UdevDb"] = labels
 
     attach_dmi(report)
-
-    # Use the hardware information to create a machine type.
-    if "dmi.sys.vendor" in report and "dmi.product.name" in report:
-        report[
-            "MachineType"
-        ] = "{report['dmi.sys.vendor']} {report['dmi.product.name']}"
 
     if command_available("prtconf"):
         report["Prtconf"] = command_output(["prtconf"])

tests/unit/test_hookutils.py
@@ -83,6 +83,7 @@ class TestHookutils(unittest.TestCase):
                 "dmi.product.family": "B550 MB",
                 "dmi.product.name": "B550I AORUS PRO AX",
                 "dmi.sys.vendor": "Gigabyte Technology Co., Ltd.",
+                "MachineType": "Gigabyte Technology Co., Ltd. B550I AORUS PRO AX",
             },
         )
 