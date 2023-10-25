canonical/apport

commit 4a38b722974426ba2bd134775358b1d381d17c49

Increase maximum line length to 88
Use the default line length from black. It reduces the pressure to keep
the lines short, but still keeps the lines readable. See
https://black.readthedocs.io/en/stable/the_black_code_style/current_style.html#line-length
for more reasoning.

Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

apport/crashdb.py
@@ -87,18 +87,14 @@ class CrashDatabase:
         self.format_version = 3
 
         init = (
-            not os.path.exists(path)
-            or path == ":memory:"
-            or os.path.getsize(path) == 0
+            not os.path.exists(path) or path == ":memory:" or os.path.getsize(path) == 0
         )
         self.duplicate_db = dbapi2.connect(path, timeout=7200)
 
         if init:
             cur = self.duplicate_db.cursor()
             cur.execute("CREATE TABLE version (format INTEGER NOT NULL)")
-            cur.execute(
-                "INSERT INTO version VALUES (?)", [self.format_version]
-            )
+            cur.execute("INSERT INTO version VALUES (?)", [self.format_version])
 
             cur.execute(
                 """CREATE TABLE crashes (
@@ -160,9 +156,7 @@ class CrashDatabase:
         it can be explicitly passed to this function if it is already
         available.
         """
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         if not report:
             report = self.download(crash_id)
@@ -197,8 +191,7 @@ class CrashDatabase:
             if (
                 not ex_ver
                 or not report_package_version
-                or packaging.compare_versions(report_package_version, ex_ver)
-                < 0
+                or packaging.compare_versions(report_package_version, ex_ver) < 0
             ):
                 master_id = ex_id
                 master_ver = ex_ver
@@ -246,9 +239,7 @@ class CrashDatabase:
         # if we don't have one already
         if sig:
             cur = self.duplicate_db.cursor()
-            cur.execute(
-                "SELECT count(*) FROM crashes WHERE crash_id == ?", [crash_id]
-            )
+            cur.execute("SELECT count(*) FROM crashes WHERE crash_id == ?", [crash_id])
             count_id = cur.fetchone()[0]
             if count_id == 0:
                 cur.execute(
@@ -341,9 +332,7 @@ class CrashDatabase:
         version specifies the package version the crash was fixed in (None for
         'still unfixed').
         """
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         cur = self.duplicate_db.cursor()
         n = cur.execute(
@@ -360,22 +349,16 @@ class CrashDatabase:
 
         This happens when a report got rejected or manually duplicated.
         """
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         cur = self.duplicate_db.cursor()
         cur.execute("DELETE FROM crashes WHERE crash_id = ?", [crash_id])
-        cur.execute(
-            "DELETE FROM address_signatures WHERE crash_id = ?", [crash_id]
-        )
+        cur.execute("DELETE FROM address_signatures WHERE crash_id = ?", [crash_id])
         self.duplicate_db.commit()
 
     def duplicate_db_change_master_id(self, old_id, new_id):
         """Change a crash ID."""
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         cur = self.duplicate_db.cursor()
         cur.execute(
@@ -407,9 +390,7 @@ class CrashDatabase:
         then moved to the given name in an almost atomic way.
         """
         # hard to change, pylint: disable=consider-using-with
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         # first create the temporary new dir; if that fails, nothing has been
         # changed and we fail early
@@ -449,9 +430,7 @@ class CrashDatabase:
         cur_hash = None
         cur_file = None
 
-        cur.execute(
-            "SELECT signature, crash_id from crashes ORDER BY signature"
-        )
+        cur.execute("SELECT signature, crash_id from crashes ORDER BY signature")
         for sig, crash_id in cur.fetchall():
             h = self.duplicate_sig_hash(sig)
             if h is None:
@@ -480,9 +459,7 @@ class CrashDatabase:
         # Format 3 added a primary key which can't be done as an upgrade in
         # SQLite
         if cur_format < 3:
-            raise SystemError(
-                "Cannot upgrade database from format earlier than 3"
-            )
+            raise SystemError("Cannot upgrade database from format earlier than 3")
 
         cur = self.duplicate_db.cursor()
 
@@ -538,8 +515,7 @@ class CrashDatabase:
         cur = self.duplicate_db.cursor()
 
         cur.execute(
-            "SELECT crash_id FROM address_signatures WHERE signature == ?",
-            [sig],
+            "SELECT crash_id FROM address_signatures WHERE signature == ?", [sig]
         )
         existing_ids = cur.fetchall()
         assert len(existing_ids) <= 1
@@ -558,9 +534,7 @@ class CrashDatabase:
 
         This is mainly useful for debugging and test suites.
         """
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         dump = {}
         cur = self.duplicate_db.cursor()
@@ -580,14 +554,10 @@ class CrashDatabase:
         An invalid ID gets removed from the duplicate db, and a crash which got
         fixed is marked as such in the database.
         """
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         cur = self.duplicate_db.cursor()
-        cur.execute(
-            "SELECT fixed_version FROM crashes WHERE crash_id = ?", [crash_id]
-        )
+        cur.execute("SELECT fixed_version FROM crashes WHERE crash_id = ?", [crash_id])
         db_fixed_version = cur.fetchone()
         if not db_fixed_version:
             return
@@ -597,10 +567,7 @@ class CrashDatabase:
 
         # crash got rejected
         if real_fixed_version == "invalid":
-            print(
-                f"DEBUG: bug {crash_id} was invalidated,"
-                f" removing from database"
-            )
+            print(f"DEBUG: bug {crash_id} was invalidated," f" removing from database")
             self.duplicate_db_remove(crash_id)
             return
 
@@ -634,8 +601,7 @@ class CrashDatabase:
         else:
             cur = self.duplicate_db.cursor()
             cur.execute(
-                "INSERT INTO address_signatures VALUES (?, ?)",
-                (_u(sig), crash_id),
+                "INSERT INTO address_signatures VALUES (?, ?)", (_u(sig), crash_id)
             )
             self.duplicate_db.commit()
 
@@ -645,9 +611,7 @@ class CrashDatabase:
         This is necessary when having to mark a bug as a duplicate if it
         already is in the duplicate DB.
         """
-        assert (
-            self.duplicate_db
-        ), "init_duplicate_db() needs to be called before"
+        assert self.duplicate_db, "init_duplicate_db() needs to be called before"
 
         cur = self.duplicate_db.cursor()
         cur.execute("DELETE FROM crashes WHERE crash_id = ?", [dup])
@@ -680,9 +644,7 @@ class CrashDatabase:
     # Abstract functions that need to be implemented by subclasses
     #
 
-    def upload(
-        self, report, progress_callback=None, user_message_callback=None
-    ):
+    def upload(self, report, progress_callback=None, user_message_callback=None):
         """Upload given problem report return a handle for it.
 
         This should happen noninteractively.
@@ -936,9 +898,7 @@ def get_crashdb(auth_file, name=None, conf=None):
       'bug_pattern_url', 'dupdb_url', and 'problem_types'.
     """
     if not conf:
-        conf = os.environ.get(
-            "APPORT_CRASHDB_CONF", "/etc/apport/crashdb.conf"
-        )
+        conf = os.environ.get("APPORT_CRASHDB_CONF", "/etc/apport/crashdb.conf")
     settings = {}
     with open(conf, encoding="utf-8") as f:
         # legacy, pylint: disable=exec-used
@@ -953,10 +913,7 @@ def get_crashdb(auth_file, name=None, conf=None):
                 try:
                     with open(cfpath, encoding="utf-8") as f:
                         # legacy, pylint: disable=exec-used
-                        exec(
-                            compile(f.read(), cfpath, "exec"),
-                            settings["databases"],
-                        )
+                        exec(compile(f.read(), cfpath, "exec"), settings["databases"])
                 except Exception as error:  # pylint: disable=broad-except
                     # ignore broken files
                     sys.stderr.write(f"Invalid file {cfpath}: {error}\n")
@@ -973,9 +930,6 @@ def load_crashdb(auth_file, spec):
     spec is a crash db configuration dictionary as described in get_crashdb().
     """
     m = __import__(
-        f"apport.crashdb_impl.{spec['impl']}",
-        globals(),
-        locals(),
-        ["CrashDatabase"],
+        f"apport.crashdb_impl.{spec['impl']}", globals(), locals(), ["CrashDatabase"]
     )
     return m.CrashDatabase(auth_file, spec)

apport/fileutils.py
@@ -107,9 +107,9 @@ def find_package_desktopfile(package):
     for line in packaging.get_files(package):
         if line.endswith(".desktop"):
             # restrict to autostart and applications, see LP#1147528
-            if not line.startswith(
-                "/etc/xdg/autostart"
-            ) and not line.startswith("/usr/share/applications/"):
+            if not line.startswith("/etc/xdg/autostart") and not line.startswith(
+                "/usr/share/applications/"
+            ):
                 continue
 
             if desktopfile:
@@ -429,9 +429,7 @@ def make_report_file(report, uid=None):
     elif "Package" in report:
         subject = report["Package"].split(None, 1)[0]
     else:
-        raise ValueError(
-            "report has neither ExecutablePath nor Package attribute"
-        )
+        raise ValueError("report has neither ExecutablePath nor Package attribute")
 
     if not uid:
         uid = os.geteuid()
@@ -586,9 +584,7 @@ def get_process_environ(proc_pid_fd: int) -> dict[str, str]:
 
     if not environ:
         return {}
-    return dict(
-        [entry.split("=", 1) for entry in environ.split("\0") if "=" in entry]
-    )
+    return dict([entry.split("=", 1) for entry in environ.split("\0") if "=" in entry])
 
 
 def get_process_path(proc_pid_fd=None):
@@ -601,9 +597,7 @@ def get_process_path(proc_pid_fd=None):
         return "unknown"
 
 
-def get_core_path(
-    pid=None, exe=None, uid=None, timestamp=None, proc_pid_fd=None
-):
+def get_core_path(pid=None, exe=None, uid=None, timestamp=None, proc_pid_fd=None):
     """Get the path to a core file."""
     if pid is None:
         pid = "unknown"
@@ -702,9 +696,7 @@ def shared_libraries(path):
     return libs
 
 
-def should_skip_crash(
-    report: ProblemReport, filename: str
-) -> typing.Optional[str]:
+def should_skip_crash(report: ProblemReport, filename: str) -> typing.Optional[str]:
     """Check if the crash should be skipped for flood protection.
 
     In case the crash should be skipped return a string with the reason.
@@ -715,10 +707,7 @@ def should_skip_crash(
     except (KeyError, ValueError):
         crash_counter = 0
     if crash_counter > 1:
-        return (
-            f"this executable already crashed"
-            f" {crash_counter} times, ignoring"
-        )
+        return f"this executable already crashed" f" {crash_counter} times, ignoring"
     if os.path.exists(filename) and not seen_report(filename):
         # don't clobber existing report
         return (

commit 99bc6a9676ac54398ae03514a22c7cb63cfee7bf

style: merge consecutive f-strings
Black merged several multi-line f-strings into one line leaving two
f-string in the line that could be merged.

Fixes: 4a38b722974426ba2bd134775358b1d381d17c49 ("Increase maximum line length to 88")
Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

apport/crashdb.py
@@ -569,7 +569,7 @@ class CrashDatabase:
 
         # crash got rejected
         if real_fixed_version == "invalid":
-            print(f"DEBUG: bug {crash_id} was invalidated," f" removing from database")
+            print(f"DEBUG: bug {crash_id} was invalidated, removing from database")
             self.duplicate_db_remove(crash_id)
             return
 

apport/fileutils.py
@@ -708,7 +708,7 @@ def should_skip_crash(report: ProblemReport, filename: str) -> Optional[str]:
     except (KeyError, ValueError):
         crash_counter = 0
     if crash_counter > 1:
-        return f"this executable already crashed" f" {crash_counter} times, ignoring"
+        return f"this executable already crashed {crash_counter} times, ignoring"
     if os.path.exists(filename) and not seen_report(filename):
         # don't clobber existing report
         return (

apport/packaging_impl/apt_dpkg.py
@@ -479,9 +479,7 @@ class __AptDpkgPackageInfo(PackageInfo):
     def get_modified_files(self, package):
         """Return list of all modified files of a package."""
         # get the maximum mtime of package files that we consider unmodified
-        listfile = (
-            f"/var/lib/dpkg/info/{package}" f":{self.get_system_architecture()}.list"
-        )
+        listfile = f"/var/lib/dpkg/info/{package}:{self.get_system_architecture()}.list"
         if not os.path.exists(listfile):
             listfile = f"/var/lib/dpkg/info/{package}.list"
         try:
@@ -495,7 +493,7 @@ class __AptDpkgPackageInfo(PackageInfo):
         # create a list of files with a newer timestamp for md5sum'ing
         sums = b""
         sumfile = (
-            f"/var/lib/dpkg/info/{package}" f":{self.get_system_architecture()}.md5sums"
+            f"/var/lib/dpkg/info/{package}:{self.get_system_architecture()}.md5sums"
         )
         if not os.path.exists(sumfile):
             sumfile = f"/var/lib/dpkg/info/{package}.md5sums"
@@ -972,7 +970,7 @@ class __AptDpkgPackageInfo(PackageInfo):
                 try:
                     cache_pkg = cache[pkg]
                 except KeyError:
-                    m = f"package {pkg.replace('%', '%%')} does not exist," f" ignoring"
+                    m = f"package {pkg.replace('%', '%%')} does not exist, ignoring"
                     obsolete += f"{m}\n"
                     apport.logging.warning("%s", m)
                     continue
@@ -1007,7 +1005,7 @@ class __AptDpkgPackageInfo(PackageInfo):
             try:
                 cache_pkg = cache[pkg]
             except KeyError:
-                m = f"package {pkg.replace('%', '%%')} does not exist," f" ignoring"
+                m = f"package {pkg.replace('%', '%%')} does not exist, ignoring"
                 obsolete += f"{m}\n"
                 apport.logging.warning("%s", m)
                 continue
@@ -1257,9 +1255,7 @@ class __AptDpkgPackageInfo(PackageInfo):
                                     lp_cache[dbgsym_pkg] = ver
                                     pkg_found = True
                             if not pkg_found:
-                                obsolete += (
-                                    f"no debug symbol package found" f" for {pkg}\n"
-                                )
+                                obsolete += f"no debug symbol package found for {pkg}\n"
 
         # unpack packages, weed out the ones that are already installed (for
         # permanent sandboxes)

apport/report.py
@@ -1652,9 +1652,9 @@ class Report(problem_report.ProblemReport):
                     parsed = re.search("^BUG: unable to handle (.*) at ", line)
                     if parsed:
                         match = parsed.group(1)
-                        assert match, (
-                            f"could not parse expected" f" problem type line: {line}"
-                        )
+                        assert (
+                            match
+                        ), f"could not parse expected problem type line: {line}"
                         parts.append(match)
 
                 if line.startswith("IP: "):
@@ -1887,7 +1887,7 @@ class Report(problem_report.ProblemReport):
                     "GCONV_PATH": f"{gdb_sandbox}/usr/lib/{native_multiarch}/gconv",
                 }
                 command.insert(
-                    0, f"{gdb_sandbox}" f"/lib/{native_multiarch}/ld-linux-x86-64.so.2"
+                    0, f"{gdb_sandbox}/lib/{native_multiarch}/ld-linux-x86-64.so.2"
                 )
                 command += ["--ex", f"set data-directory {gdb_sandbox}/usr/share/gdb"]
             if not os.path.exists(sandbox + executable):

apport/ui.py
@@ -814,9 +814,7 @@ class UserInterface:
                 if not os.path.exists(
                     os.path.join(apport.report.PACKAGE_HOOK_DIR, f"source_{p}.py")
                 ):
-                    print(
-                        f"Package {p} not installed and no hook available," f" ignoring"
-                    )
+                    print(f"Package {p} not installed and no hook available, ignoring")
                     continue
             self.collect_info(ignore_uninstalled=True)
             info_collected = True

data/general-hooks/parse_segv.py
@@ -392,7 +392,7 @@ def main():
     if len(sys.argv) != 4 or sys.argv[1] in ["-h", "--help"]:
         print("To run self-test, run without any arguments (or with -v)")
         print("To do stand-alone crash parsing:")
-        print(f"  Usage:" f" {sys.argv[0]} Registers.txt Disassembly.txt ProcMaps.txt")
+        print(f"  Usage: {sys.argv[0]} Registers.txt Disassembly.txt ProcMaps.txt")
         sys.exit(0)
 
     with open(sys.argv[1], encoding="utf-8") as registers_file:

problem_report.py
@@ -457,7 +457,7 @@ class ProblemReport(collections.UserDict):
                 if len(v) >= 4 and v[3]:
                     if size == 0:
                         raise OSError(
-                            f"did not get any data" f" for field {k} from {str(v[0])}"
+                            f"did not get any data for field {k} from {str(v[0])}"
                         )
 
             # flush compressor and write the rest

tests/integration/test_report.py
@@ -49,7 +49,7 @@ class T(unittest.TestCase):
             elapsed_time += 0.1
 
         self.fail(
-            f"/proc/{pid}/cmdline not readable within" f" {int(elapsed_time)} seconds."
+            f"/proc/{pid}/cmdline not readable within {int(elapsed_time)} seconds."
         )
 
     @unittest.mock.patch("time.sleep")

tests/integration/test_signal_crashes.py
@@ -1081,9 +1081,7 @@ class T(unittest.TestCase):
 
         args = " ".join(f" {a}" for a in args)
         if uid is not None:
-            args = (
-                f" --reuid={uid} --clear-groups " f"/bin/sh -c 'exec {command}{args}'"
-            )
+            args = f" --reuid={uid} --clear-groups /bin/sh -c 'exec {command}{args}'"
             command = "/usr/bin/setpriv"
             gdb_args += ["--ex", "set follow-fork-mode child"]
 
@@ -1100,7 +1098,7 @@ class T(unittest.TestCase):
     def _get_report_filename(command: str) -> str:
         return os.path.join(
             apport.fileutils.report_dir,
-            f"{os.path.realpath(command).replace('/', '_')}" f".{os.getuid()}.crash",
+            f"{os.path.realpath(command).replace('/', '_')}.{os.getuid()}.crash",
         )
 
     def check_report_coredump(self, report_path):
@@ -1128,7 +1126,7 @@ class T(unittest.TestCase):
             timeout += 0.1
         else:
             self.fail(
-                f"Core file {core_file} not created " f"within {int(timeout)} seconds."
+                f"Core file {core_file} not created within {int(timeout)} seconds."
             )
 
         gdb_process = psutil.Process(gdb_pid)

tests/integration/test_ui.py
@@ -964,7 +964,7 @@ class T(unittest.TestCase):
         self.assertTrue(len(self.ui.report["CoreDump"]) > 10000)
         self.assertTrue(
             self.ui.report["Title"].startswith(
-                f"{os.path.basename(self.TEST_EXECUTABLE)}" f" crashed with SIGSEGV"
+                f"{os.path.basename(self.TEST_EXECUTABLE)} crashed with SIGSEGV"
             )
         )
 
@@ -1549,7 +1549,7 @@ class T(unittest.TestCase):
 
             self.assertTrue(
                 self.ui.report["Title"].startswith(
-                    f"{os.path.basename(self.TEST_EXECUTABLE)}" f" crashed with SIGSEGV"
+                    f"{os.path.basename(self.TEST_EXECUTABLE)} crashed with SIGSEGV"
                 ),
                 self.ui.report["Title"],
             )

tests/system/test_packaging_apt_dpkg.py
@@ -1078,7 +1078,7 @@ def _copy_ubuntu_keyrings(keyring_dir: str) -> None:
     dbgsym_keyring = "/usr/share/keyrings/ubuntu-dbgsym-keyring.gpg"
     if not os.path.isfile(dbgsym_keyring):
         pytest.skip(  # pragma: no cover
-            f"{dbgsym_keyring} missing. " f"Please install ubuntu-dbgsym-keyring!"
+            f"{dbgsym_keyring} missing. Please install ubuntu-dbgsym-keyring!"
         )
     # Convert from GPG keybox database format to OpenPGP Public Key format
     output_dbgsym_keyring = os.path.join(keyring_dir, os.path.basename(dbgsym_keyring))