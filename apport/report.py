canonical/apport

commit 44eba9f1123c052a2705ca4d314f392d01fd7c5a

Fix path traversal vulnerability with hooks execution
Ensure that Package: and SourcePackage: fields loaded from reports do not
contain directories. Until now, an attacker could trick a user into opening a⎵
malicious .crash file containing

  Package: ../../../../some/dir/foo

which would execute /some/dir/foo.py with arbitrary code.

Note: This did not work for SourcePackage: as this would require a⎵
/usr/share/apport/*-hooks/source_/ directory -- but sanity-check the value
anyway as a second line of defence.

Thanks to Donncha O'Cearbhaill for discovering this!

CVE-2016-9950
LP: #1648806

apport/report.py
@@ -828,14 +828,21 @@ class Report(problem_report.ProblemReport):
         False otherwise.
         '''
         # determine package names, unless already given as arguments
+        # avoid path traversal
         if not package:
             package = self.get('Package')
         if package:
             package = package.split()[0]
+            if '/' in package:
+                self['UnreportableReason'] = 'invalid Package: %s' % package
+                return
         if not srcpackage:
             srcpackage = self.get('SourcePackage')
         if srcpackage:
             srcpackage = srcpackage.split()[0]
+            if '/' in srcpackage:
+                self['UnreportableReason'] = 'invalid SourcePackage: %s' % package
+                return
 
         hook_dirs = [_hook_dir]
         # also search hooks in /opt, when program is from there

test/test_ui.py
@@ -951,6 +951,26 @@ bOgUs=
         self.assertFalse(os.path.exists('/tmp/pwned'))
         self.assertIn('invalid crash database definition', self.ui.msg_text)
 
+    def test_run_crash_malicious_package(self):
+        '''Package: path traversal'''
+
+        bad_hook = tempfile.NamedTemporaryFile(suffix='.py')
+        bad_hook.write(b"def add_info(r, u):\n  open('/tmp/pwned', 'w').close()")
+        bad_hook.flush()
+
+        self.report['ExecutablePath'] = '/bin/bash'
+        self.report['Package'] = '../' * 20 + os.path.splitext(bad_hook.name)[0]
+        self.update_report_file()
+        self.ui.present_details_response = {'report': True,
+                                            'blacklist': False,
+                                            'examine': False,
+                                            'restart': False}
+
+        self.ui.run_crash(self.report_file.name)
+
+        self.assertFalse(os.path.exists('/tmp/pwned'))
+        self.assertIn('invalid Package:', self.ui.msg_text)
+
     def test_run_crash_ignore(self):
         '''run_crash() on a crash with the Ignore field'''
         self.report['Ignore'] = 'True'

commit 8f7f2b63ff6433085c527baae255064a9ef80215

report: fix reporting invalid SourcePackage
Coverity noticed a copy-paste error: The reported invalid source package
name should refer to the source package name instead of the binary
package name.

Fixes: 44eba9f1123c ("Fix path traversal vulnerability with hooks execution")
Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

apport/report.py
@@ -1140,7 +1140,7 @@ class Report(problem_report.ProblemReport):
         if srcpackage:
             srcpackage = srcpackage.split()[0]
             if "/" in srcpackage:
-                self["UnreportableReason"] = f"invalid SourcePackage: {package}"
+                self["UnreportableReason"] = f"invalid SourcePackage: {srcpackage}"
                 return False
 
         hook_dirs = [PACKAGE_HOOK_DIR]

tests/unit/test_report.py
@@ -1345,6 +1345,16 @@ No symbol table info available.
         expected = "suspend/resume:Cray XT5"
         self.assertEqual(expected, pr.crash_signature())
 
+    def test_add_hooks_info_invalid_source(self) -> None:
+        """Test Report.add_hooks_info() with invalid source package name."""
+        report = apport.report.Report()
+        result = report.add_hooks_info(package="bash", srcpackage="in/valid")
+
+        self.assertFalse(result)
+        self.assertEqual(
+            report.get("UnreportableReason"), "invalid SourcePackage: in/valid"
+        )
+
     def test_add_snap_contact_info_launchpad(self):
         """add_snap_contact_info()
 