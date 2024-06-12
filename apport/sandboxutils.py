canonical/apport

commit 52560e691cb140ad37b3bf147e17a4d6fdbd2029

apport-retrace: install base-files first
On Ubuntu 24.04 (noble):

```
$ divide-by-zero
$ mkdir -p "$PWD/sandbox-config/Ubuntu 24.04"
$ echo noble > "$PWD/sandbox-config/Ubuntu 24.04/codename"
$ cat > "$PWD/sandbox-config/Ubuntu 24.04/sources.list" <<EOF
deb http://de.archive.ubuntu.com/ubuntu/ noble main universe
deb-src http://de.archive.ubuntu.com/ubuntu/ noble main universe
EOF
$ apport-retrace -v -S "$PWD/sandbox-config" --gdb-sandbox -C "$PWD/sandbox-cache" /var/crash/_usr_bin_divide-by-zero.1000.crash
[...]
Extracting downloaded debs...
ERROR: [Errno 2] No such file or directory: '/tmp/apport_sandbox_8x704zqr/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2'
```

Bug-Ubuntu: https://launchpad.net/bugs/2067120

apport/sandboxutils.py
@@ -120,6 +120,17 @@ def needed_runtime_packages(report, pkgmap_cache_dir, pkg_versions, verbose=Fals
     return [(p, pkg_versions.get(p)) for p in pkgs]
 
 
+def _move_or_add_base_files_first(pkgs: list[tuple[str, None | str]]) -> None:
+    """Move base-files to the front or add it if missing."""
+    base_files_version = None
+    for i, (pkg, version) in enumerate(pkgs):
+        if pkg == "base-files":
+            base_files_version = version
+            pkgs.pop(i)
+            break
+    pkgs[:0] = [("base-files", base_files_version)]
+
+
 # pylint: disable-next=too-many-arguments
 def make_sandbox(
     report: apport.Report,
@@ -221,6 +232,9 @@ def make_sandbox(
         origins = set(match.findall(pkg_list))
         if origins:
             apport.logging.log(f"Origins: {origins}")
+
+    # Install base-files first to get correct usrmerge
+    _move_or_add_base_files_first(pkgs)
 
     # unpack packages, if any, using cache and sandbox
     try:

tests/unit/test_sandboxutils.py
@@ -17,7 +17,7 @@ from unittest.mock import MagicMock
 
 from apport.packaging import PackageInfo
 from apport.report import Report
-from apport.sandboxutils import make_sandbox
+from apport.sandboxutils import _move_or_add_base_files_first, make_sandbox
 
 
 class TestSandboxutils(unittest.TestCase):
@@ -71,3 +71,36 @@ class TestSandboxutils(unittest.TestCase):
             self.assertEqual(cache, cache_dir)
         self.assertEqual(outdated_msg, "obsolete\nobsolete\n")
         self.assertEqual(packaging_mock.install_packages.call_count, 2)
+
+    def test_move_or_add_base_files_first_existing(self) -> None:
+        """_move_or_add_base_files_first() with base-files in list."""
+        pkgs: list[tuple[str, None | str]] = [
+            ("chaos-marmosets", "0.1.2-2"),
+            ("base-files", "13ubuntu9"),
+            ("libc6", "2.39-0ubuntu8.2"),
+        ]
+        _move_or_add_base_files_first(pkgs)
+        self.assertEqual(
+            pkgs,
+            [
+                ("base-files", "13ubuntu9"),
+                ("chaos-marmosets", "0.1.2-2"),
+                ("libc6", "2.39-0ubuntu8.2"),
+            ],
+        )
+
+    def test_move_or_add_base_files_first_missing(self) -> None:
+        """_move_or_add_base_files_first() without base-files in list."""
+        pkgs: list[tuple[str, None | str]] = [
+            ("chaos-marmosets", "0.1.2-2"),
+            ("libc6", "2.39-0ubuntu8.2"),
+        ]
+        _move_or_add_base_files_first(pkgs)
+        self.assertEqual(
+            pkgs,
+            [
+                ("base-files", None),
+                ("chaos-marmosets", "0.1.2-2"),
+                ("libc6", "2.39-0ubuntu8.2"),
+            ],
+        )

commit 670083b2ff3c589c8db83df1b93d3f2a42cd1c22

apport-retrace: avoid installing base-files unconditionally
Installing `base-files` is not needed after applying the proper fix for
finding `ld-linux-x86-64.so.2`. So revert installing `base-files` unless
it was requested. In that case install it as first package to corretly
set-up the usrmerge symlinks.

Bug-Ubuntu: https://launchpad.net/bugs/2067120
Fixes: 52560e691cb1 ("apport-retrace: install base-files first")

apport/sandboxutils.py
@@ -120,15 +120,13 @@ def needed_runtime_packages(report, pkgmap_cache_dir, pkg_versions, verbose=Fals
     return [(p, pkg_versions.get(p)) for p in pkgs]
 
 
-def _move_or_add_base_files_first(pkgs: list[tuple[str, None | str]]) -> None:
+def _move_base_files_first(pkgs: list[tuple[str, None | str]]) -> None:
     """Move base-files to the front or add it if missing."""
-    base_files_version = None
     for i, (pkg, version) in enumerate(pkgs):
         if pkg == "base-files":
-            base_files_version = version
             pkgs.pop(i)
-            break
-    pkgs[:0] = [("base-files", base_files_version)]
+            pkgs[:0] = [("base-files", version)]
+            return
 
 
 # pylint: disable-next=too-many-arguments
@@ -234,7 +232,7 @@ def make_sandbox(
             apport.logging.log(f"Origins: {origins}")
 
     # Install base-files first to get correct usrmerge
-    _move_or_add_base_files_first(pkgs)
+    _move_base_files_first(pkgs)
 
     # unpack packages, if any, using cache and sandbox
     try:

tests/unit/test_sandboxutils.py
@@ -17,7 +17,7 @@ from unittest.mock import MagicMock
 
 from apport.packaging import PackageInfo
 from apport.report import Report
-from apport.sandboxutils import _move_or_add_base_files_first, make_sandbox
+from apport.sandboxutils import _move_base_files_first, make_sandbox
 
 
 class TestSandboxutils(unittest.TestCase):
@@ -72,14 +72,14 @@ class TestSandboxutils(unittest.TestCase):
         self.assertEqual(outdated_msg, "obsolete\nobsolete\n")
         self.assertEqual(packaging_mock.install_packages.call_count, 2)
 
-    def test_move_or_add_base_files_first_existing(self) -> None:
-        """_move_or_add_base_files_first() with base-files in list."""
+    def test_move_base_files_first_existing(self) -> None:
+        """_move_base_files_first() with base-files in list."""
         pkgs: list[tuple[str, None | str]] = [
             ("chaos-marmosets", "0.1.2-2"),
             ("base-files", "13ubuntu9"),
             ("libc6", "2.39-0ubuntu8.2"),
         ]
-        _move_or_add_base_files_first(pkgs)
+        _move_base_files_first(pkgs)
         self.assertEqual(
             pkgs,
             [
@@ -89,18 +89,13 @@ class TestSandboxutils(unittest.TestCase):
             ],
         )
 
-    def test_move_or_add_base_files_first_missing(self) -> None:
-        """_move_or_add_base_files_first() without base-files in list."""
+    def test_move_base_files_first_missing(self) -> None:
+        """_move_base_files_first() without base-files in list."""
         pkgs: list[tuple[str, None | str]] = [
             ("chaos-marmosets", "0.1.2-2"),
             ("libc6", "2.39-0ubuntu8.2"),
         ]
-        _move_or_add_base_files_first(pkgs)
+        _move_base_files_first(pkgs)
         self.assertEqual(
-            pkgs,
-            [
-                ("base-files", None),
-                ("chaos-marmosets", "0.1.2-2"),
-                ("libc6", "2.39-0ubuntu8.2"),
-            ],
+            pkgs, [("chaos-marmosets", "0.1.2-2"), ("libc6", "2.39-0ubuntu8.2")]
         )