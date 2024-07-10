f8bb2939b3d3d04265a2eea7cead233b7e56b14f
Benjamin Drung <benjamin.drung@canonical.com>
2024-07-10 17:30:05 +0200

    apport-retrace: exit without 0 on error
    Exit `apport-retrace` with return code 2 in case the report is malformed
    and with return code 3 on known errors. Crashes of `apport-retrace` will
    exit with return code 1.
    
    Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

---
+++ tests/unit/test_apport_retrace.py
@@ -0,0 +1,51 @@
+"""Unit tests for apport-retrace."""
+
+import io
+import tempfile
+import unittest
+from unittest.mock import MagicMock
+
+from tests.helper import import_module_from_file
+from tests.paths import get_bin_directory
+
+apport_retrace = import_module_from_file(get_bin_directory() / "apport-retrace")
+
+
+@unittest.mock.patch.object(apport_retrace, "get_crashdb")
+def test_malformed_crash_report(get_crashdb_mock: MagicMock) -> None:
+    """Test apport-retrace to fail on malformed crash report."""
+    with (
+        tempfile.NamedTemporaryFile(mode="w+", suffix=".crash") as crash_file,
+        unittest.mock.patch("sys.stderr", new_callable=io.StringIO) as stderr,
+    ):
+        crash_file.write(
+            "ProblemType: Crash\nArchitecture: amd64\nPackage: gedit 46.2-2\n"
+        )
+        crash_file.flush()
+        return_code = apport_retrace.main(["-x", "/usr/bin/gedit", crash_file.name])
+
+    assert return_code == 2
+    assert (
+        stderr.getvalue()
+        == "ERROR: report file does not contain one of the required fields:"
+        " CoreDump DistroRelease\n"
+    )
+    get_crashdb_mock.assert_called_once_with(None)
+
+
+@unittest.mock.patch.object(apport_retrace, "get_crashdb")
+def test_malformed_kernel_crash_report(get_crashdb_mock: MagicMock) -> None:
+    """Test apport-retrace to fail on malformed kernel crash report."""
+    with (
+        tempfile.NamedTemporaryFile(mode="w+", suffix=".crash") as crash_file,
+        unittest.mock.patch("sys.stderr", new_callable=io.StringIO) as stderr,
+    ):
+        crash_file.write("ProblemType: KernelCrash\n")
+        crash_file.flush()
+        return_code = apport_retrace.main([crash_file.name])
+
+    assert return_code == 2
+    assert (
+        stderr.getvalue() == "ERROR: report file does not contain the required fields\n"
+    )
+    get_crashdb_mock.assert_called_once_with(None)
