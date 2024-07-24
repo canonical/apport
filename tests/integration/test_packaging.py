16f157ebd542c8f6adbbc748aa7b6a8b8c02e2fc
Benjamin Drung <benjamin.drung@canonical.com>
2024-07-24 21:12:21 +0200

    packaging: let get_uninstalled_package always return str
    `PackageInfo.get_uninstalled_package` is only used in the test suite.
    The tests that use `get_uninstalled_package` expect it to return a valid
    name. The code path that returns `None` is currently not hit.
    
    Let `PackageInfo.get_uninstalled_package` always return a package and
    raise a `ValueError` in case no uninstalled package was found. Otherwise
    the callers would need to check for the possible `None` result.
    
    Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

--- tests/integration/test_packaging.py
+++ tests/integration/test_packaging.py
@@ -14,7 +14,6 @@ class T(unittest.TestCase):
     def test_get_uninstalled_package(self) -> None:
         """get_uninstalled_package()"""
         p = apport.packaging.get_uninstalled_package()
-        self.assertIsNotNone(p)
         self.assertNotEqual(apport.packaging.get_available_version(p), "")
         self.assertRaises(ValueError, apport.packaging.get_version, p)
         self.assertTrue(apport.packaging.is_distro_package(p))
