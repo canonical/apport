96de0b4d68e1d0ca78d4f3268571966c2286d30c
Benjamin Drung <benjamin.drung@canonical.com>
2024-07-31 23:55:16 +0200

    apt_dpkg: use SQLite3 for path to package mapping
    Bug: https://launchpad.net/bugs/2073787
    Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

--- tests/unit/test_packaging_apt_dpkg.py
+++ tests/unit/test_packaging_apt_dpkg.py
@@ -9,6 +9,8 @@
 
 """Unit tests for apport.packaging_impl.apt_dpkg."""
 
+import pathlib
+import sqlite3
 import tempfile
 import unittest
 import unittest.mock
@@ -20,6 +22,7 @@ import apt.package
 from apport.packaging_impl.apt_dpkg import (
     _map_mirror_to_arch,
     _parse_deb822_sources,
+    _Path2Package,
     _read_mirror_file,
     impl,
 )
@@ -175,16 +178,15 @@ Components: main
         )
 
     @unittest.mock.patch.object(impl, "_get_file2pkg_mapping")
-    @unittest.mock.patch.object(impl, "_save_contents_mapping", MagicMock())
     def test_get_file_package_uninstalled_usrmerge(
         self, _get_file2pkg_mapping_mock: MagicMock
     ) -> None:
         """get_file_package() on uninstalled usrmerge packages."""
         # Data from Ubuntu 24.04 (noble)
         _get_file2pkg_mapping_mock.return_value = {
-            b"usr/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2": b"libc6",
-            b"usr/lib/x86_64-linux-gnu/libc.so.6": b"libc6",
-            b"usr/libx32/libc.so.6": b"libc6-x32",
+            "usr/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2": "libc6",
+            "usr/lib/x86_64-linux-gnu/libc.so.6": "libc6",
+            "usr/libx32/libc.so.6": "libc6-x32",
         }
 
         pkg = impl.get_file_package(
@@ -200,7 +202,7 @@ Components: main
         """Test _update_given_file2pkg_mapping skipping xenial Contents header."""
         # Header taken from
         # http://archive.ubuntu.com/ubuntu/dists/xenial/Contents-amd64.gz
-        contents = b"""\
+        contents = """\
 This file maps each file available in the Ubuntu
 system to the package from which it originates.  It includes packages
 from the DIST distribution for the ARCH architecture.
@@ -237,22 +239,21 @@ FILE                                                    LOCATION
 bin/afio						    multiverse/utils/afio
 bin/archdetect						    utils/archdetect-deb
 """
-        file2pkg: dict[bytes, bytes] = {}
+        file2pkg = _Path2Package()
         open_mock = unittest.mock.mock_open(read_data=contents)
         with unittest.mock.patch("gzip.open", open_mock):
-            # pylint: disable-next=protected-access
-            impl._update_given_file2pkg_mapping(file2pkg, "/fake_Contents", "xenial")
+            file2pkg.update_from_contents_file("/fake_Contents", "xenial", 2)
 
         self.assertEqual(
-            file2pkg, {b"bin/afio": b"afio", b"bin/archdetect": b"archdetect-deb"}
+            dict(file2pkg), {"bin/afio": "afio", "bin/archdetect": "archdetect-deb"}
         )
-        open_mock.assert_called_once_with("/fake_Contents", "rb")
+        open_mock.assert_called_once_with("/fake_Contents", "rt")
 
     def test_contents_path_filering(self) -> None:
         """Test _update_given_file2pkg_mapping to ignore unrelevant files."""
         # Test content taken from
         # http://archive.ubuntu.com/ubuntu/dists/noble/Contents-amd64.gz
-        contents = b"""\
+        contents = """\
 bin/ip							    net/iproute2
 boot/ipxe.efi						    admin/grub-ipxe
 etc/dput.cf						    devel/dput
@@ -283,14 +284,13 @@ usr/src/broadcom-sta.tar.xz				    restricted/admin/broadcom-sta-source
 var/lib/ieee-data/iab.txt				    net/ieee-data
 """
 
-        file2pkg: dict[bytes, bytes] = {}
+        file2pkg = _Path2Package()
         open_mock = unittest.mock.mock_open(read_data=contents)
         with unittest.mock.patch("gzip.open", open_mock):
-            # pylint: disable-next=protected-access
-            impl._update_given_file2pkg_mapping(file2pkg, "Contents-amd64", "noble")
+            file2pkg.update_from_contents_file("Contents-amd64", "noble", 10)
 
         self.assertEqual(
-            {k.decode(): v.decode() for k, v in file2pkg.items()},
+            dict(file2pkg),
             {
                 "bin/ip": "iproute2",
                 "etc/dput.cf": "dput",
@@ -309,7 +309,7 @@ var/lib/ieee-data/iab.txt				    net/ieee-data
                 "usr/share/dicom3tools/gen.so": "dicom3tools",
             },
         )
-        open_mock.assert_called_once_with("Contents-amd64", "rb")
+        open_mock.assert_called_once_with("Contents-amd64", "rt")
 
     def test_contents_parse_path_with_spaces(self) -> None:
         """Test _update_given_file2pkg_mapping to parse Contents file correctly."""
@@ -322,21 +322,71 @@ var/lib/ieee-data/iab.txt				    net/ieee-data
             "/__init__.py universe/python/ilorest\n"
         )
 
-        file2pkg: dict[bytes, bytes] = {}
-        open_mock = unittest.mock.mock_open(read_data=contents.encode())
+        file2pkg = _Path2Package()
+        open_mock = unittest.mock.mock_open(read_data=contents)
         with unittest.mock.patch("gzip.open", open_mock):
-            # pylint: disable-next=protected-access
-            impl._update_given_file2pkg_mapping(file2pkg, "Contents-amd64", "noble")
+            file2pkg.update_from_contents_file("Contents-amd64", "noble")
 
         self.assertEqual(
-            {k.decode(): v.decode() for k, v in file2pkg.items()},
+            dict(file2pkg),
             {
                 "usr/lib/iannix/Tools/JavaScript Library.js": "iannix",
                 "usr/lib/python3/dist-packages/ilorest/extensions/BIOS COMMANDS"
                 "/__init__.py": "ilorest",
             },
         )
-        open_mock.assert_called_once_with("Contents-amd64", "rb")
+        open_mock.assert_called_once_with("Contents-amd64", "rt")
+
+    def test_path2package(self) -> None:
+        """Basic tests for _Path2Package class."""
+        path2package = _Path2Package()
+        self.assertEqual(dict(path2package), {})
+        self.assertEqual(len(path2package), 0)
+        self.assertTrue(path2package.is_empty())
+        path2package["usr/bin/man"] = "man-db"
+        path2package["bin/ip"] = "iproute2"
+        self.assertEqual(
+            dict(path2package), {"bin/ip": "iproute2", "usr/bin/man": "man-db"}
+        )
+        self.assertEqual(len(path2package), 2)
+        self.assertEqual(path2package["bin/ip"], "iproute2")
+        self.assertEqual(path2package.get("usr/bin/man"), "man-db")
+        self.assertIsNone(path2package.get("usr/src/broadcom-sta.tar.xz"))
+        self.assertFalse(path2package.is_empty())
+        self.assertIn("bin/ip", path2package)
+        self.assertNotIn("usr/bin/git", path2package)
+        self.assertNotIn(42, path2package)
+
+    def test_path2package_overwrite(self) -> None:
+        """Test _Path2Package to overwrite existing values."""
+        path2package = _Path2Package()
+        path2package["path/to/file"] = "package1"
+        self.assertEqual(path2package["path/to/file"], "package1")
+        path2package["path/to/file"] = "package2"
+        self.assertEqual(path2package["path/to/file"], "package2")
+
+    def test_path2package_create_and_reopen(self) -> None:
+        """Test _Path2Package for openening an existing database."""
+        with tempfile.NamedTemporaryFile(suffix=".sqlite3") as db_file:
+            path2package = _Path2Package(pathlib.Path(db_file.name))
+            self.assertEqual(dict(path2package), {})
+            self.assertTrue(path2package.is_empty())
+            path2package["bin/ip"] = "iproute2"
+            path2package.connection.commit()
+            del path2package
+
+            path2package = _Path2Package(pathlib.Path(db_file.name))
+            self.assertEqual(dict(path2package), {"bin/ip": "iproute2"})
+            self.assertFalse(path2package.is_empty())
+
+    @unittest.mock.patch("sqlite3.connect")
+    def test_path2package_clean_deletion_on_failure(
+        self, connect_mock: MagicMock
+    ) -> None:
+        """Test clean _Path2Package deletion on __init__ failure."""
+        connect_mock.side_effect = sqlite3.OperationalError
+        with self.assertRaises(sqlite3.OperationalError):
+            _Path2Package(pathlib.Path("/non-existing.sqlite3"))
 
 
 class TestPackaging(unittest.TestCase):
