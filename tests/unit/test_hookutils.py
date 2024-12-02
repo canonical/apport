454fffba910133899e02b18fb27a660c59f72aba
Benjamin Drung <benjamin.drung@canonical.com>
2024-12-02 14:01:19 +0100

    hookutils: replace iwconfig call by iw dev link calls
    The `wireless-tools` package that ships the `iwconfig` userspace utility
    has been removed in Ubuntu 24.10 "oracular" (see LP #2075850).
    
    Replace the `iwconfig` call by `iw dev $device link` calls which should
    provide the most equivalent results.
    
    Bug: https://launchpad.net/bugs/2090790

--- tests/unit/test_hookutils.py
+++ tests/unit/test_hookutils.py
@@ -10,6 +10,52 @@ from unittest.mock import MagicMock, Mock
 
 import apport.hookutils
 
+IW_REG_LIST_DE = b"""\
+global
+country 00: DFS-UNSET
+	(755 - 928 @ 2), (N/A, 20), (N/A), PASSIVE-SCAN
+	(2402 - 2472 @ 40), (N/A, 20), (N/A)
+	(2457 - 2482 @ 20), (N/A, 20), (N/A), AUTO-BW, PASSIVE-SCAN
+	(2474 - 2494 @ 20), (N/A, 20), (N/A), NO-OFDM, PASSIVE-SCAN
+	(5170 - 5250 @ 80), (N/A, 20), (N/A), AUTO-BW, PASSIVE-SCAN
+	(5250 - 5330 @ 80), (N/A, 20), (0 ms), DFS, AUTO-BW, PASSIVE-SCAN
+	(5490 - 5730 @ 160), (N/A, 20), (0 ms), DFS, PASSIVE-SCAN
+	(5735 - 5835 @ 80), (N/A, 20), (N/A), PASSIVE-SCAN
+	(57240 - 63720 @ 2160), (N/A, 0), (N/A)
+
+phy#0 (self-managed)
+country DE: DFS-UNSET
+	(2402 - 2437 @ 40), (6, 22), (N/A), AUTO-BW, NO-HT40MINUS, NO-80MHZ, NO-160MHZ
+	(2422 - 2462 @ 40), (6, 22), (N/A), AUTO-BW, NO-80MHZ, NO-160MHZ
+	(2447 - 2482 @ 40), (6, 22), (N/A), AUTO-BW, NO-HT40PLUS, NO-80MHZ, NO-160MHZ
+	(5170 - 5190 @ 160), (6, 22), (N/A), NO-OUTDOOR, AUTO-BW, IR-CONCURRENT, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5190 - 5210 @ 160), (6, 22), (N/A), NO-OUTDOOR, AUTO-BW, IR-CONCURRENT, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5210 - 5230 @ 160), (6, 22), (N/A), NO-OUTDOOR, AUTO-BW, IR-CONCURRENT, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5230 - 5250 @ 160), (6, 22), (N/A), NO-OUTDOOR, AUTO-BW, IR-CONCURRENT, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5250 - 5270 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5270 - 5290 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5290 - 5310 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5310 - 5330 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5490 - 5510 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5510 - 5530 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5530 - 5550 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5550 - 5570 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5570 - 5590 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5590 - 5610 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5610 - 5630 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-320MHZ, PASSIVE-SCAN
+	(5630 - 5650 @ 160), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-320MHZ, PASSIVE-SCAN
+	(5650 - 5670 @ 80), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-160MHZ, NO-320MHZ, PASSIVE-SCAN
+	(5670 - 5690 @ 80), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-160MHZ, NO-320MHZ, PASSIVE-SCAN
+	(5690 - 5710 @ 80), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40MINUS, NO-160MHZ, NO-320MHZ, PASSIVE-SCAN
+	(5710 - 5730 @ 80), (6, 22), (0 ms), DFS, AUTO-BW, NO-HT40PLUS, NO-160MHZ, NO-320MHZ, PASSIVE-SCAN
+	(5735 - 5755 @ 80), (6, 22), (N/A), AUTO-BW, NO-HT40MINUS, NO-160MHZ, NO-320MHZ
+	(5755 - 5775 @ 80), (6, 22), (N/A), AUTO-BW, NO-HT40PLUS, NO-160MHZ, NO-320MHZ
+	(5775 - 5795 @ 80), (6, 22), (N/A), AUTO-BW, NO-HT40MINUS, NO-160MHZ, NO-320MHZ
+	(5795 - 5815 @ 80), (6, 22), (N/A), AUTO-BW, NO-HT40PLUS, NO-160MHZ, NO-320MHZ
+	(5815 - 5835 @ 20), (6, 22), (N/A), AUTO-BW, NO-HT40MINUS, NO-HT40PLUS, NO-80MHZ, NO-160MHZ, NO-320MHZ
+
+"""  # noqa
+
 
 class TestHookutils(unittest.TestCase):
     # pylint: disable=missing-function-docstring
@@ -143,6 +189,109 @@ class TestHookutils(unittest.TestCase):
             run_mock.call_args[0][0],
             ["journalctl", "--priority=warning", "-b", "--lines=1000"],
         )
+
+    @unittest.mock.patch("glob.glob")
+    @unittest.mock.patch("os.path.exists")
+    @unittest.mock.patch("subprocess.run")
+    @unittest.mock.patch(
+        "apport.hookutils.recent_syslog", MagicMock(return_value="some recent logs")
+    )
+    def test_attach_wifi(
+        self, run_mock: MagicMock, exists_mock: MagicMock, glob_mock: MagicMock
+    ) -> None:
+        """Test attach_wifi() with some real world data from Ubuntu 25.04.
+
+        This test case does not test the recent_syslog() call."""
+        glob_mock.return_value = ["/sys/class/net/wlp3s0/wireless"]
+        exists_mock.side_effect = [True, False]
+        rfkill_list = (
+            b"0: hci0: Bluetooth\n	Soft blocked: no\n	Hard blocked: no\n"
+            b"1: phy0: Wireless LAN\n	Soft blocked: no\n	Hard blocked: no\n"
+        )
+        iw_dev_wlp3s0_link_params = (
+            b"	freq: 2437.0\n"
+            b"	RX: 2363829 bytes (3994 packets)\n"
+            b"	TX: 303619 bytes (1560 packets)\n"
+            b"	signal: -43 dBm\n"
+            b"	rx bitrate: 286.7 MBit/s HE-MCS 11 HE-NSS 2 HE-GI 0 HE-DCM 0\n"
+            b"	tx bitrate: 286.7 MBit/s HE-MCS 11 HE-NSS 2 HE-GI 0 HE-DCM 0\n"
+            b"	bss flags: CTS-protection short-preamble short-slot-time\n"
+            b"	dtim period: 2\n"
+            b"	beacon int: 100\n"
+        )
+        iw_dev_wlp3s0_link = (
+            b"Connected to 00:53:e9:15:6c:33 (on wlp3s0)\n	SSID: MyWifiNetwork\n"
+            + iw_dev_wlp3s0_link_params
+        )
+        run_mock.side_effect = [
+            subprocess.CompletedProcess(
+                args=MagicMock(), returncode=0, stdout=rfkill_list, stderr=b""
+            ),
+            subprocess.CompletedProcess(
+                args=MagicMock(), returncode=0, stdout=iw_dev_wlp3s0_link, stderr=b""
+            ),
+            subprocess.CompletedProcess(
+                args=MagicMock(), returncode=0, stdout=IW_REG_LIST_DE, stderr=b""
+            ),
+        ]
+
+        report = apport.Report()
+        apport.hookutils.attach_wifi(report)
+
+        self.assertEqual(report["WifiSyslog"], "some recent logs")
+        self.assertEqual(report["RfKill"], rfkill_list.decode("utf-8").rstrip())
+        self.assertEqual(
+            report["IwDevWlp3s0Link"],
+            "Connected to <hidden-mac> (on wlp3s0)\n	SSID: <hidden>\n"
+            + iw_dev_wlp3s0_link_params.decode("utf-8").rstrip(),
+        )
+        self.assertEqual(report["CRDA"], IW_REG_LIST_DE.decode("utf-8").rstrip())
+        self.assertEqual(
+            set(report.keys()),
+            {"CRDA", "Date", "IwDevWlp3s0Link", "ProblemType", "RfKill", "WifiSyslog"},
+        )
+        glob_mock.assert_called_once_with("/sys/class/net/*/wireless")
+        self.assertEqual(
+            [call.args[0] for call in exists_mock.call_args_list],
+            ["/sbin/iw", "/var/log/wpa_supplicant.log"],
+        )
+        self.assertEqual(
+            [call.args[0] for call in run_mock.call_args_list],
+            [["rfkill", "list"], ["iw", "dev", "wlp3s0", "link"], ["iw", "reg", "get"]],
+        )
+
+    @unittest.mock.patch("os.path.exists")
+    @unittest.mock.patch("subprocess.run")
+    @unittest.mock.patch(
+        "apport.hookutils.recent_syslog", MagicMock(return_value="some recent logs")
+    )
+    def test_attach_wifi_without_iw(
+        self, run_mock: MagicMock, exists_mock: MagicMock
+    ) -> None:
+        """Test attach_wifi() with no iw command installed.
+
+        This test case does not test the recent_syslog() call and rfkill command."""
+        exists_mock.return_value = False
+        run_mock.return_value = subprocess.CompletedProcess(
+            args=MagicMock(), returncode=0, stdout=b"rfkill output", stderr=b""
+        )
+
+        report = apport.Report()
+        apport.hookutils.attach_wifi(report)
+
+        self.assertEqual(report["WifiSyslog"], "some recent logs")
+        self.assertEqual(report["RfKill"], "rfkill output")
+        self.assertEqual(report["CRDA"], "N/A")
+        self.assertEqual(
+            set(report.keys()), {"CRDA", "Date", "ProblemType", "RfKill", "WifiSyslog"}
+        )
+        exists_mock.assert_called()
+        self.assertEqual(
+            [call.args[0] for call in exists_mock.call_args_list],
+            ["/sbin/iw", "/var/log/wpa_supplicant.log"],
+        )
+        run_mock.assert_called_once()
+        self.assertEqual(run_mock.call_args_list[0].args[0], ["rfkill", "list"])
 
     def test_path_to_key(self) -> None:
         """Transform a file path to a valid report key."""
