canonical/apport

commit 370f30dd9c358f9468d6c4ef911b86a0b5a2359e

apport/hookutils.py: Gather dmesg output as root
Use root_command_output to gather dmesg rather than failing to include it.

Import changes from apport 2.20.11-0ubuntu46 and 2.20.11-0ubuntu61.

LP: #1896095

apport/hookutils.py
@@ -201,7 +201,7 @@ def attach_dmesg(report):
     This will not overwrite already existing information.
     '''
     if not report.get('CurrentDmesg', '').strip():
-        report['CurrentDmesg'] = command_output(['dmesg'])
+        report['CurrentDmesg'] = root_command_output(['dmesg'])
 
 
 def attach_dmi(report):
@@ -729,8 +729,8 @@ def attach_mac_events(report, profiles=None):
     aa_re = re.compile(aa_regex, re.IGNORECASE)
 
     if 'KernLog' not in report:
-        report['KernLog'] = __filter_re_process(
-            mac_re, subprocess.Popen(['dmesg'], stdout=subprocess.PIPE))
+        report['KernLog'] = '\n'.join(re.findall(
+            mac_re, root_command_output(['dmesg'])))
 
     if 'AuditLog' not in report and os.path.exists('/var/run/auditd.pid'):
         attach_root_command_outputs(report, {'AuditLog': 'egrep "' + mac_regex + '" /var/log/audit/audit.log'})

commit 6cae24599b689904d45fc9281777f375a1aa5d44

hookutils: attach_mac_events: fix dmesg capture regex
Co-authored-by: Benjamin Drung <benjamin.drung@canonical.com>
Co-authored-by: Bryce Harrington <bryce.harrington@canonical.com>
Fixes: 370f30dd9c358f9468d6c4ef911b86a0b5a2359e ("apport/hookutils.py: Gather dmesg output as root")
Bug-Ubuntu: https://bugs.launchpad.net/ubuntu/+source/apport/+bug/2090887

apport/hookutils.py
@@ -910,7 +910,7 @@ def attach_mac_events(report, profiles=None):
     if isinstance(profiles, str):
         profiles = [profiles]
 
-    mac_regex = r"audit\(|apparmor|selinux|security"
+    mac_regex = r"(?:audit\(|apparmor|selinux|security).*"
     mac_re = re.compile(mac_regex, re.IGNORECASE)
     aa_regex = 'apparmor="DENIED".+?profile=([^ ]+?)[ ]'
     aa_re = re.compile(aa_regex, re.IGNORECASE)