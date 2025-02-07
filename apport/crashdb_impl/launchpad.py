canonical/apport

commit d27a3c68ac96d0d9e879d776a9cde5ec61f80f53

launchpad: force CRLF as HTTP line separators
The Python email library explicitly says both its default and legacy
policies do not follow the RFC:

> The default is \n because that’s the internal end-of-line discipline
> used by Python, though \r\n is required by the RFCs.

This is why we cannot have nice things.

It was fine until Launchpad updated its multipart implementation, which
became stricter when parsing input. They've temporarily reverted the
update, since it broke our error reporting, but we're the ones in the
wrong.

Now, there's the bigger question of whether it's appropriate to use the
email module to construct a fairly standard HTTP request, and thus bake
some obscure knowledge of the HTTP protocol into our code.

The requests library would have given us the needed APIs to generate a
multipart/form-data message, but we've deliberately moved away from it
to reduce the apport footprint.

We could try adding the headers to the Request object rather than the
MIME object, but that wouldn't remove the need for the module entirely,
and my experience with email in Python is that it's a fickle beast that
one shouldn't touch more than strictly necessary.

Bug-Ubuntu: https://bugs.launchpad.net/ubuntu/+source/apport/+bug/2096327

apport/crashdb_impl/launchpad.py
@@ -1236,7 +1236,8 @@ def upload_blob(blob, progress_callback=None, hostname="launchpad.net"):
 
     data_flat = io.BytesIO()
     gen = email.generator.BytesGenerator(data_flat, mangle_from_=False)
-    gen.flatten(data)
+    # HTTP 1.1 mandates CRLF line separators
+    gen.flatten(data, linesep="\r\n")
 
     # do the request; we need to explicitly set the content type here, as it
     # defaults to x-www-form-urlencoded

tests/unit/test_crashdb_launchpad.py
@@ -9,9 +9,11 @@
 
 """Unit tests for apport.crash_impl.launchpad"""
 
+import io
 import re
+import unittest.mock
 
-from apport.crashdb_impl.launchpad import CrashDatabase
+from apport.crashdb_impl.launchpad import CrashDatabase, upload_blob
 from apport.report import Report
 
 
@@ -91,3 +93,24 @@ def test_private_bug_headers() -> None:
 
     assert headers.get("Private") == "yes"
     assert not re.search(r"need-[a-z0-9]+-retrace", headers.get("Tags", ""))
+
+
+@unittest.mock.patch("urllib.request.build_opener")
+def test_upload_blob_crlf(builder_mock: unittest.mock.MagicMock) -> None:
+    """Test that upload_blob encodes form data with CRLF as line separators,
+    as per HTTP 1.1 spec"""
+    builder_mock.return_value.open.return_value.info.return_value = {
+        "X-Launchpad-Blob-Token": 42
+    }
+
+    data = io.BytesIO(b"foobarbaz")
+    result = upload_blob(data, hostname="invalid.host")
+
+    builder_mock.assert_called_once()
+    builder_mock.return_value.open.assert_called_once()
+    req = builder_mock.return_value.open.call_args[0][0]
+    assert b"foobarbaz" in req.data
+    assert re.match(
+        rb'Content-Type: multipart/mixed; boundary="=+[0-9]+=+"\r\n', req.data
+    )
+    assert result == 42

commit 45b12c63173c984127ccc2dc4837ad7285a79d8d

Sort of revert the HTTP 1.1 CRLF business
It turns out that Launchpad itself isn't ready to cope with CRLF line
endings.

Fixes: d27a3c68ac "launchpad: force CRLF as HTTP line separators"
Bug-Ubuntu: https://bugs.launchpad.net/launchpad/+bug/2097168

apport/crashdb_impl/launchpad.py
@@ -1236,8 +1236,8 @@ def upload_blob(blob, progress_callback=None, hostname="launchpad.net"):
 
     data_flat = io.BytesIO()
     gen = email.generator.BytesGenerator(data_flat, mangle_from_=False)
-    # HTTP 1.1 mandates CRLF line separators
-    gen.flatten(data, linesep="\r\n")
+    # HTTP 1.1 mandates CRLF line separators but LP prefers LF for now (LP: #2097632)
+    gen.flatten(data, linesep="\n")
 
     # do the request; we need to explicitly set the content type here, as it
     # defaults to x-www-form-urlencoded

tests/unit/test_crashdb_launchpad.py
@@ -96,9 +96,9 @@ def test_private_bug_headers() -> None:
 
 
 @unittest.mock.patch("urllib.request.build_opener")
-def test_upload_blob_crlf(builder_mock: unittest.mock.MagicMock) -> None:
-    """Test that upload_blob encodes form data with CRLF as line separators,
-    as per HTTP 1.1 spec"""
+def test_upload_blob_conform_to_lp(builder_mock: unittest.mock.MagicMock) -> None:
+    """Test that upload_blob does NOT encodes form data with CRLF as line separators,
+    despite HTTP 1.1 spec"""
     builder_mock.return_value.open.return_value.info.return_value = {
         "X-Launchpad-Blob-Token": 42
     }
@@ -110,7 +110,7 @@ def test_upload_blob_crlf(builder_mock: unittest.mock.MagicMock) -> None:
     builder_mock.return_value.open.assert_called_once()
     req = builder_mock.return_value.open.call_args[0][0]
     assert b"foobarbaz" in req.data
-    assert re.match(
-        rb'Content-Type: multipart/mixed; boundary="=+[0-9]+=+"\r\n', req.data
-    )
+    # Check that we have LF-separated headers (because LP expects LF
+    # line separators, see LP: #2097632)
+    assert re.match(rb"^([-A-Za-z]+: [^\r\n]+\n)+\n", req.data)
     assert result == 42