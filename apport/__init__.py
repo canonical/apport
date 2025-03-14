ac00d8d68d5c00e9d3fb8840f290a979a8092657
Benjamin Drung <benjamin.drung@canonical.com>
2025-03-14 17:38:38 +0100

    Lazy load elements in base apport module
    For faster module loading and to avoid circular dependencies, load all
    elements in the base `apport` module in a lazy fashion.

--- apport/__init__.py
+++ apport/__init__.py
@@ -3,9 +3,8 @@
 # for faster module loading and avoiding circular dependencies
 # pylint: disable=import-outside-toplevel
 
-from apport.logging import error, fatal, log, memdbg, warning
-from apport.packaging_impl import impl as packaging
-from apport.report import Report
+# Sadly importing apport.packaging is needed here to shadow it later
+import apport.packaging
 
 __all__ = [
     "Report",
@@ -17,6 +16,45 @@ __all__ = [
     "unicode_gettext",
     "warning",
 ]
+
+
+# wraps an object; pylint: disable-next=invalid-name
+def Report(*args, **kwargs):
+    """Lazy loading of apport.report.Report()."""
+    from apport.report import Report as cls
+
+    return cls(*args, **kwargs)
+
+
+# wrapper object; pylint: disable-next=too-few-public-methods
+class _LazyLoadingPackaging:
+    def __getattribute__(self, name):
+        # The packaging object will be replaced by the imported object.
+        # pylint: disable-next=global-statement
+        global packaging
+        # pylint: disable-next=redefined-outer-name
+        from apport.packaging_impl import impl as packaging
+
+        return packaging.__getattribute__(name)
+
+
+packaging = _LazyLoadingPackaging()
+
+
+def _logging_function(function_name):
+    def _wrapped_logging_function(*args, **kwargs):
+        from apport import logging
+
+        return getattr(logging, function_name)(*args, **kwargs)
+
+    return _wrapped_logging_function
+
+
+error = _logging_function("error")
+fatal = _logging_function("fatal")
+log = _logging_function("log")
+memdbg = _logging_function("memdbg")
+warning = _logging_function("warning")
 
 
 def unicode_gettext(message):
