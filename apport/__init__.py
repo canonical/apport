5d9c9df74c17161b681690f9baa1ea947a893a43
Benjamin Drung <benjamin.drung@canonical.com>
2025-03-14 17:38:38 +0100

    Lazy load elements in base apport module
    For faster module loading and to avoid circular dependencies, load all
    elements in the base `apport` module in a lazy fashion.

--- apport/__init__.py
+++ apport/__init__.py
@@ -3,10 +3,10 @@
 # for faster module loading and avoiding circular dependencies
 # pylint: disable=import-outside-toplevel
 
+from typing import TYPE_CHECKING
+
 # Import apport.packaging to shadow it afterwards.
 import apport.packaging as _  # noqa: F401
-from apport.packaging_impl import impl as packaging
-from apport.report import Report
 
 __all__ = [
     "Report",
@@ -18,6 +18,29 @@ __all__ = [
     "unicode_gettext",
     "warning",
 ]
+
+if TYPE_CHECKING:
+    from apport.packaging_impl import impl as packaging
+    from apport.report import Report
+else:
+    # wraps an object; pylint: disable-next=invalid-name
+    def Report(*args, **kwargs):
+        """Lazy loading of apport.report.Report()."""
+        from apport.report import Report as cls
+
+        return cls(*args, **kwargs)
+
+    # wrapper object; pylint: disable-next=too-few-public-methods
+    class _LazyLoadingPackaging:
+        def __getattribute__(self, name):
+            # The packaging object will be replaced by the imported object.
+            # pylint: disable-next=global-statement
+            global packaging
+            from apport.packaging_impl import impl as packaging
+
+            return packaging.__getattribute__(name)
+
+    packaging = _LazyLoadingPackaging()
 
 
 def _logging_function(function_name):
