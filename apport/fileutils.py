2457ec1d6f1f909857a40c87521d8d2f1e2a1f19
Benjamin Drung <benjamin.drung@canonical.com>
2025-04-16 14:33:34 +0200

    fileutils: respect proc_pid_fd in get_core_path
    `get_core_path` does not use `proc_pid_fd` to determine `timestamp` (in
    case `timestamp` is not provided to the function). This opens this
    function for attacks on `/proc/<pid>`.
    
    CVE-2025-5054
    Bug: https://launchpad.net/bugs/2107472

--- apport/fileutils.py
+++ apport/fileutils.py
@@ -605,8 +605,16 @@ def get_core_path(
     proc_pid_fd: int | None = None,
 ) -> tuple[str, str]:
     """Get the path to a core file."""
+
+    def opener(path: str | os.PathLike[str], flags: int) -> int:
+        return os.open(path, flags, dir_fd=proc_pid_fd)
+
     if pid is not None and timestamp is None:
-        with open(f"/proc/{pid}/stat", encoding="utf-8") as stat_file:
+        if proc_pid_fd is not None:
+            stat_filename = "stat"
+        else:
+            stat_filename = f"/proc/{pid}/stat"
+        with open(stat_filename, encoding="utf-8", opener=opener) as stat_file:
             stat_contents = stat_file.read()
         timestamp = get_starttime(stat_contents)
 
