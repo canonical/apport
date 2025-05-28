canonical/apport

commit d350eeca938c2667a52fe115d23400111dd112dd

Initial commit for 'reports from application hangs' branch.

apport/ui.py
@@ -18,6 +18,7 @@ __version__ = '2.2.3'
 import glob, sys, os.path, optparse, traceback, locale, gettext
 import errno, zlib
 import subprocess, threading, webbrowser
+import signal
 
 import apport, apport.fileutils, apport.REThread
 
@@ -304,6 +305,40 @@ class UserInterface:
             else:
                 raise
 
+    def run_hang(self, pid):
+        '''Report an application hanging.
+
+        This will first present a dialog containing the information it can
+        collect from the running application (everything but the trace) with
+        the option of terminating or restarting the application, optionally
+        reporting that this error occurred.
+
+        A SEGV will then be sent to the process and a series of noninteractive
+        processes will collect the remaining information, append it to the
+        report generated here, potentially restart the application, and then
+        mark that report for uploading.
+        '''
+        self.report = apport.Report('Hang')
+        self.report.add_proc_info(pid)
+        self.report.add_package_info()
+        self.cur_package = apport.fileutils.find_file_package(self.report.get('ExecutablePath', ''))
+        self.report.add_os_info()
+        allowed_to_report = apport.fileutils.allowed_to_report()
+        response = self.ui_present_report_details(allowed_to_report)
+        if not response['report']:
+            return
+        if response['restart']:
+            self.report['NeedsRestart'] = '1'
+        path = self.report['ExecutablePath'].replace('/', '_')
+        reportfile = '%s/%s.%i.crash' % (apport.fileutils.report_dir,
+                                         path, os.getuid())
+        with open(reportfile, 'wb') as f:
+            self.report.write(f)
+        self.kill_segv(pid)
+
+    def kill_segv(self, pid):
+        os.kill(int(pid), signal.SIGSEGV)
+
     def run_report_bug(self, symptom_script=None):
         '''Report a bug.
 
@@ -557,6 +592,9 @@ class UserInterface:
         if self.options.symptom:
             self.run_symptom()
             return True
+        elif self.options.pid and self.options.hanging:
+            self.run_hang(self.options.pid)
+            return True
         elif self.options.filebug:
             return self.run_report_bug()
         elif self.options.update_report is not None:
@@ -648,6 +686,8 @@ class UserInterface:
             help=_('Specify package name in --file-bug mode. This is optional if a --pid is specified. (Implied if package name is given as only argument.)'))
         optparser.add_option('-P', '--pid', type='int',
             help=_('Specify a running program in --file-bug mode. If this is specified, the bug report will contain more information.  (Implied if pid is given as only argument.)'))
+        optparser.add_option('--hanging', action='store_true', default=False,
+            help=_('The provided pid is a hanging application.'))
         optparser.add_option('-c', '--crash-file', metavar='PATH',
             help=_('Report the crash from given .apport or .crash file instead of the pending ones in %s. (Implied if file is given as only argument.)') % apport.fileutils.report_dir)
         optparser.add_option('--save', metavar='PATH',

data/apport
@@ -365,6 +365,7 @@ try:
 
     crash_counter = 0
 
+    application_hang = False
     # Create crash report file descriptor for writing the report into
     # report_dir
     try:
@@ -382,14 +383,27 @@ try:
                     sys.exit(1)
                 # remove the old file, so that we can create the new one with
                 # os.O_CREAT|os.O_EXCL
-                os.unlink(report)
+                if not application_hang:
+                    os.unlink(report)
             else:
                 error_log('apport: report %s already exists and unseen, doing nothing to avoid disk usage DoS' % report)
                 drop_privileges(pid)
                 write_user_coredump(pid, cwd, core_ulimit)
                 sys.exit(1)
-        reportfile = os.fdopen(os.open(report,
-            os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0), 'wb')
+
+            with open(report, 'rb') as f:
+                r = apport.Report()
+                r.load(f)
+                try:
+                    if r['ProblemType'] == 'Hang':
+                        application_hang = True
+                except KeyError:
+                    pass
+        if application_hang:
+            reportfile = os.fdopen(os.open(report, os.O_WRONLY, 0), 'wb')
+        else:
+            reportfile = os.fdopen(os.open(report,
+                os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0), 'wb')
         assert reportfile.fileno() > sys.stderr.fileno()
 
         # Make sure the crash reporting daemon can read this report

gtk/apport-gtk
@@ -195,7 +195,25 @@ class GTKUserInterface(apport.ui.UserInterface):
         self.w('continue_button').set_label(_('Continue'))
 
         self.w('cancel_button').hide()
-        if not self.report_file:
+        if report_type == 'Hang':
+            self.w('ignore_future_problems').set_active(False)
+            self.w('ignore_future_problems').hide()
+            self.w('closed_button').show()
+            self.w('closed_button').set_label(_('Force Closed'))
+            self.w('continue_button').set_label(_('Relaunch'))
+            self.w('subtitle_label').hide()
+            self.desktop_info = self.get_desktop_entry()
+            if self.desktop_info:
+                icon = self.desktop_info.get('icon')
+                name = self.desktop_info['name']
+                name = GLib.markup_escape_text(name)
+                title = _('The application %s has stopped responding.') % name
+            else:
+                icon = 'distributor-logo'
+                name = os.path.basename(self.report['ExecutablePath'])
+                title = _('The program "%s" has stopped responding.') % name
+            self.w('title_label').set_label('<big><b>%s</b></big>' % title)
+        elif not self.report_file:
             self.setup_bug_report()
         elif report_type == 'KernelCrash' or report_type == 'KernelOops':
             self.w('ignore_future_problems').set_active(False)

commit abfebe8adddc5658fc0b7435df03ed9d739eab57

ui: fix return value for --hanging case in run_argv
The docstring for the method `UserInterface.run_argv` says that the
method returns `True` if at least one report has been processed, and
`False` otherwise. In case `UserInterface.run_hang` returns `False` no
report has been processed.

Use the return value from `UserInterface.run_hang` in `run_argv`. There
seems to be no consumer of the return value of `run_argv`.

Fixes: d350eeca938 ("Initial commit for 'reports from application hangs' branch.")

apport/ui.py
@@ -915,8 +915,7 @@ class UserInterface:
             self.run_symptom()
             return True
         if self.args.hanging:
-            self.run_hang(self.args.pid)
-            return True
+            return self.run_hang(self.args.pid)
         if self.args.filebug:
             return self.run_report_bug()
         if self.args.update_report is not None: